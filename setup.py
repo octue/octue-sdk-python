9b934b1944a3609bc9c42e6d658b6b9e5d136509
Tom Clark <thc29@MacBook-Pro-2.local>
2018-03-20 11:35:53 +0000

    Updated setup.py to work as jsut a module, with a TODO to move out certain commands to the example app template.

--- setup.py
+++ setup.py
@@ -1,12 +1,22 @@
-from setuptools import setup
+from distutils.core import setup
 
-setup(
-    name="octue",
-    version=0.1,
-    py_modules=['cli'],
-    install_requires=['Click'],
-    entry_points='''
-    [console_scripts]
-    octue=octue_app
-    ''',
-)
+
+setup(name='octue-sdk-python',
+      version='0.1',
+      packages=['octue', 'octue.utils', 'octue.config', 'octue.manifest', 'octue.resources'],
+      url='https://www.github.com/octue/octue-sdk-python',
+      license='',
+      author='thclark',
+      author_email='support@octue.com',
+      description='A python package providing a python SDK to the Octue API and providing supporting functions to Octue applications')
+
+
+# TODO move the following into octue example app template
+# setup(
+#     py_modules=['cli'],
+#     install_requires=['Click'],
+#     entry_points='''
+#     [console_scripts]
+#     octue=octue_app
+#     ''',
+# )
