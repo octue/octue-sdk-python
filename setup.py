fc125599843c5cb5ed4ebaf1492966679ad05ffd
Tom Clark <>
2019-03-01 16:33:44 +0000

    Version bump for release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('LICENSE') as f:
     license_text = f.read()
 
 setup(name='octue',
-      version='0.1.0',
+      version='0.1.2',
       py_modules=['cli'],
       install_requires=['click'],
       url='https://www.github.com/octue/octue-sdk-python',
