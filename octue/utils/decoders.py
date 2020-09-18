618837108f5ab3e34151f5776b3d454763acef08
Tom Clark <tom@octue.com>
2020-09-18 18:32:05 +0100

    IMP Added custom encoder and decoder for serialisation

---
+++ octue/utils/decoders.py
@@ -0,0 +1,30 @@
+from json import JSONDecoder
+
+from octue.resources import Datafile, Dataset, Manifest
+
+
+def default_object_hook(obj):
+    """ A hook to convert default json objects into their Datafile, Dataset or Manifest class as appropriate
+    """
+
+    # object hooks are called whenever a json object is created. When nested, this is done from innermost (deepest
+    # nesting) out so it's safe to work at multiple levels here
+
+    if "files" in obj:
+        files = [Datafile(**df) for df in obj.pop("files")]
+        return {**obj, "files": files}
+
+    if "datasets" in obj:
+        datasets = [Dataset(**ds) for ds in obj.pop("datasets")]
+        return Manifest(**obj, datasets=datasets)
+
+    return obj
+
+
+class OctueJSONDecoder(JSONDecoder):
+    """ A JSON Decoder to convert default json objects into their Datafile, Dataset or Manifest classes as appropriate
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.object_hook = self.object_hook or default_object_hook
---
+++ octue/utils/encoders.py
@@ -0,0 +1,15 @@
+from twined.utils import TwinedEncoder
+
+
+class OctueJSONEncoder(TwinedEncoder):
+    """ A JSON Encoder which allows objects having a `serialise()` method to control their own conversion to primitives
+    """
+
+    def default(self, obj):
+
+        # If the class defines a serialise() method then use it
+        if hasattr(obj, "serialise"):
+            return obj.serialise()
+
+        # Otherwise let the base class default method raise the TypeError
+        return TwinedEncoder.default(self, obj)
