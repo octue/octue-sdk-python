dcaadb6bcef96200904ff41de65ca8a791464e83
Tom Clark <tom@octue.com>
2020-09-25 20:07:08 +0100

    TST Increase test coverage on Identifiable mixin to 100pc

--- tests/test_identifiable.py
+++ tests/test_identifiable.py
@@ -30,6 +30,13 @@ class IdentifiableTestCase(BaseTestCase):
         self.assertIsInstance(resource.id, str)
         self.assertEqual(len(resource.id), 36)
 
+    def test_repr(self):
+        """ Ensures the class instantiates without arguments
+        """
+        id = "07d38e81-6b00-4079-901b-e250ea3c7773"
+        resource = Identifiable(id=id)
+        self.assertEqual(resource.__repr__, id)
+
     def test_raises_error_with_non_uuid(self):
         """ Ensures that if a string is passed not matching the UUID pattern, that an exception is raised
         """
