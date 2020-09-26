bfaa3d69d5430de2d89791d8299713258d022c87
Tom Clark <tom@octue.com>
2020-09-26 07:50:54 +0100

    REF Removed unused modules

--- octue/utils/strip_from_end.py
+++
@@ -1,6 +0,0 @@
-def strip_from_end(text, suffix):
-    """ Strip a suffix from text, if it appears (otherwise return text unchanged)
-    """
-    if not text.endswith(suffix):
-        return text
-    return text[: len(text) - len(suffix)]
