2e59186b4138fb72bb15bdb8abd385179673b280
Tom Clark <tom@octue.com>
2020-10-03 17:38:24 +0100

    IMP Preparing output manifests

--- octue/resources/manifest.py
+++ octue/resources/manifest.py
@@ -1,6 +1,8 @@
 import logging
 
+from octue.exceptions import InvalidInputException
 from octue.mixins import Identifiable, Loggable, Serialisable
+from .dataset import Dataset
 
 
 module_logger = logging.getLogger(__name__)
@@ -17,4 +19,33 @@ class Manifest(Serialisable, Loggable, Identifiable):
         """
         super().__init__(id=id, logger=logger)
         self.datasets = kwargs.pop("datasets", list())
+        self.keys = kwargs.pop("keys", dict())
         self.__dict__.update(**kwargs)
+
+        # TODO we need to add keys to the manifest file schema so that we know what dataset(s) map to what keys
+
+    def get_dataset(self, key):
+        """ Gets a dataset by its key name (as defined in the twine)
+        """
+        idx = self.keys.get(key, None)
+        if idx is None:
+            raise InvalidInputException(
+                f"Attempted to fetch unknown dataset '{key}' from Manifest. Allowable keys are: {list(self.keys.keys())}"
+            )
+
+        return self.datasets[idx]
+
+    def prepare(self, data):
+        """ Prepare new manifest from a manifest_spec
+        """
+        if len(self.datasets) > 0:
+            raise InvalidInputException("You cannot `prepare()` a manifest already instantiated with datasets")
+
+        for idx, dataset_spec in enumerate(data):
+
+            self.keys[dataset_spec["key"]] = idx
+            # TODO generate a unique name based on the filter key, tag datasets so that the tag filters in the spec
+            #  apply automatically and generate a description of the dataset
+            self.datasets.append(Dataset(logger=self.logger))
+
+        return self
