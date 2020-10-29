0e6d53024ebcd24cf585d01ba0c3a57b0a0011e0
cortadocodes <cortado.codes@protonmail.com>
2020-10-29 13:20:37 +0000

    TST: Test help CLI command; refactor version test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,5 @@
+from click.testing import CliRunner
+
 from octue.cli import octue_cli
 from .base import BaseTestCase
 
@@ -6,5 +8,13 @@ class RunnerTestCase(BaseTestCase):
 
     def test_version(self):
         """Ensure the version command works in the CLI."""
-        with self.assertRaises(SystemExit):
-            octue_cli(['--version'])
+        result = CliRunner().invoke(octue_cli, ['--version'])
+        assert 'version' in result.output
+
+    def test_help(self):
+        """Ensure the help commands works in the CLI."""
+        help_result = CliRunner().invoke(octue_cli, ['--help'])
+        assert help_result.output.startswith('Usage')
+
+        h_result = CliRunner().invoke(octue_cli, ['-h'])
+        assert help_result.output == h_result.output
