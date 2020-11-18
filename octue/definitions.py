9545c43e75645c7b767f52f1ec4d7150d0e4ce90
Tom Clark <tom@octue.com>
2020-11-18 11:17:00 +0000

    REF Refactored definitions out so they're importable before the CLI and added filename map

---
+++ octue/definitions.py
@@ -0,0 +1,26 @@
+FOLDER_DEFAULTS = {
+    "configuration": "configuration",
+    "input": "input",
+    "tmp": "tmp",
+    "output": "output",
+}
+
+VALUES_FILENAME = "values.json"
+
+MANIFEST_FILENAME = "manifest.json"
+
+STRAND_FILENAME_MAP = {
+    "configuration_values": VALUES_FILENAME,
+    "configuration_manifest": MANIFEST_FILENAME,
+    "input_values": VALUES_FILENAME,
+    "input_manifest": MANIFEST_FILENAME,
+    "output_values": VALUES_FILENAME,
+    "output_manifest": MANIFEST_FILENAME,
+}
+
+
+# TODO this should probably be defined in twined
+OUTPUT_STRANDS = ("output_values", "output_manifest")
+
+# TODO this should probably be defined in twined
+RUN_STRANDS = ("input_values", "input_manifest", "credentials", "children")
