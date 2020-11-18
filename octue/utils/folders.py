8169370c30fe3c79ad523c0950a4cd739d4e35e5
Tom Clark <tom@octue.com>
2020-11-18 12:34:28 +0000

    REF Removed unused path hinting code, possibly for reintroduction pending #31

--- octue/utils/folders.py
+++ octue/utils/folders.py
@@ -1,9 +1,7 @@
 import os
 
-from octue import exceptions
 from octue.definitions import OUTPUT_STRANDS, STRAND_FILENAME_MAP
 from twined.twine import CHILDREN_STRANDS, CREDENTIAL_STRANDS, MANIFEST_STRANDS, MONITOR_STRANDS, SCHEMA_STRANDS
-from .isfolder import isfolder
 
 
 ALL_STRANDS = (
@@ -12,14 +10,6 @@ ALL_STRANDS = (
     *CREDENTIAL_STRANDS,
     *CHILDREN_STRANDS,
     *MONITOR_STRANDS,
-)
-
-
-FOLDERS = (
-    "configuration",
-    "input",
-    "tmp",
-    "output",
 )
 
 
@@ -43,30 +33,3 @@ def get_file_name_from_strand(strand, path):
         os.makedirs(path, exist_ok=True)
 
     return os.path.join(path, STRAND_FILENAME_MAP[strand])
-
-
-def from_path(path_hints, folders=FOLDERS):
-    """ NOT IMPLEMENTED YET - Helper to find paths to individual configurations from hints
-    TODO Fix this
-    """
-    # Set paths
-    paths = dict()
-    if isinstance(path_hints, str):
-        if not os.path.isdir(path_hints):
-            raise exceptions.FolderNotFoundException(f"Specified data folder '{path_hints}' not present")
-
-        paths = {folder: os.path.join(path_hints, folder) for folder in folders}
-
-    else:
-        if (
-            not isinstance(paths, dict)
-            or (len(paths.keys()) != len(folders))
-            or not all([k in folders for k in paths.keys()])
-        ):
-            raise exceptions.InvalidInputException(
-                f"Input 'paths' should be a dict containing directory paths with the following keys: {folders}"
-            )
-
-    # Ensure paths exist on disc??
-    for folder in FOLDERS:
-        isfolder(paths[folder], make_if_absent=True)
