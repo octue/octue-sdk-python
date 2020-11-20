0bd63a23938b497791cef8617ed7e54bddfcdd29
Tom Clark <tom@octue.com>
2020-11-20 14:18:14 +0000

    IMP New Pathable mixin as a robust way of handling file paths in manifests

--- octue/mixins/__init__.py
+++ octue/mixins/__init__.py
@@ -1,7 +1,9 @@
+from .base import MixinBase
 from .identifiable import Identifiable
 from .loggable import Loggable
+from .pathable import Pathable
 from .serialisable import Serialisable
 from .taggable import Taggable
 
 
-__all__ = "Identifiable", "Loggable", "Serialisable", "Taggable"
+__all__ = "Identifiable", "Loggable", "MixinBase", "Pathable", "Serialisable", "Taggable"
