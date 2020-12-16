c258f352c7bca5308ab495b995d812cf23b289dc
cortadocodes <cortado.codes@protonmail.com>
2020-12-16 10:31:16 +0000

    REF: Split version check into smaller functions

--- .github/workflows/scripts/check-version-consistency.py
+++ .github/workflows/scripts/check-version-consistency.py
@@ -10,40 +10,42 @@ class NotAVersionBranchException(Exception):
     pass
 
 
-def release_branch_version_matches_setup_version(package_root):
-    os.chdir(package_root)
-    process = subprocess.run(["python", os.path.join(package_root, "setup.py"), "--version"], capture_output=True)
-    setup_version = process.stdout.strip().decode("utf8")
+def get_setup_version():
+    process = subprocess.run(["python", "setup.py", "--version"], capture_output=True)
+    return process.stdout.strip().decode("utf8")
 
+
+def get_branch_name():
     process = subprocess.run(["git", "branch", "--show-current"], capture_output=True)
+    return process.stdout.strip().decode("utf8")
 
-    full_branch_name = process.stdout.strip().decode("utf8")
+
+def release_branch_version_matches_setup_version(setup_version, full_branch_name):
     branch_type, branch_name = full_branch_name.split("/")
 
     if branch_type != "release":
-        error_message = f"The branch is not a release branch: {full_branch_name!r}"
-        print(error_message)
-        raise NotAVersionBranchException(error_message)
+        raise NotAVersionBranchException(f"The branch is not a release branch: {full_branch_name!r}.")
 
-    if branch_name == setup_version:
-        print(f"Release branch name matches setup.py version: {setup_version!r}")
-        return True
-
-    print(
-        f"Release branch name does not match setup.py version: branch is {full_branch_name!r} but the setup.py version "
-        f"is {setup_version!r}"
-    )
-
-    return False
+    return branch_name == setup_version
 
 
 if __name__ == "__main__":
 
+    os.chdir(PACKAGE_ROOT)
+    setup_version = get_setup_version()
+    full_branch_name = get_branch_name()
+
     try:
-        if release_branch_version_matches_setup_version(PACKAGE_ROOT):
+        if release_branch_version_matches_setup_version(setup_version, full_branch_name):
+            print(f"Release branch name matches setup.py version: {setup_version!r}.")
             sys.exit(0)
 
+        print(
+            f"Release branch name does not match setup.py version: branch is {full_branch_name!r} but setup.py version "
+            f"is {setup_version!r}."
+        )
         sys.exit(1)
 
-    except NotAVersionBranchException:
+    except NotAVersionBranchException as e:
+        print(e.args[0])
         sys.exit(0)
