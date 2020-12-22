cd4f12e4a6edc08f7e622deb027ee1048e718c52
cortadocodes <cortado.codes@protonmail.com>
2020-12-22 18:10:19 +0000

    TST: Test Hashable

---
+++ tests/mixins/test_hashable.py
@@ -0,0 +1,45 @@
+from octue.mixins import Hashable
+from ..base import BaseTestCase
+
+
+class EmptyClass:
+    pass
+
+
+class TypeWithBLAKE3Hash:
+    blake3_hash = None
+
+
+class TypeWithIterable(Hashable):
+    ATTRIBUTES_TO_HASH = ("my_iterable",)
+
+
+class HashableTestCase(BaseTestCase):
+    def test_no_attributes_to_hash_results_in_no_hash(self):
+        """ Assert classes with Hashable mixed in but no attributes to hash give None for their hash. """
+        self.assertIsNone(EmptyClass().blake3_hash)
+
+    def test_non_hashable_type_results_in_type_error(self):
+        class MyClass(Hashable):
+            ATTRIBUTES_TO_HASH = ("a_class",)
+
+        my_class = MyClass()
+        my_class.a_class = EmptyClass()
+
+        with self.assertRaises(TypeError):
+            my_class.blake3_hash
+
+    def test_iterable_attribute_with_mixed_types_raises_value_error(self):
+
+        my_class = TypeWithIterable()
+        my_class.my_iterable = [1, 2, TypeWithBLAKE3Hash()]
+
+        with self.assertRaises(ValueError):
+            my_class.blake3_hash
+
+    def test_unsortable_iterable_attribute_raises_type_error(self):
+        my_class = TypeWithIterable()
+        my_class.my_iterable = [TypeWithBLAKE3Hash(), TypeWithBLAKE3Hash()]
+
+        with self.assertRaises(TypeError):
+            my_class.blake3_hash
