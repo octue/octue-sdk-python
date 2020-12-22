f02f5fb1bc8dab9563febdf7bf45a717f5275e3c
cortadocodes <cortado.codes@protonmail.com>
2020-12-22 16:57:27 +0000

    REF: Simplify Hashable

--- octue/mixins/hashable.py
+++ octue/mixins/hashable.py
@@ -3,6 +3,15 @@ import functools
 from blake3 import blake3
 
 
+_HASH_FUNCTIONS = {
+    str: lambda attribute: attribute,
+    int: str,
+    float: str,
+    type(None): lambda attribute: "None",
+    dict: lambda attribute: str(sorted(attribute.items())),
+}
+
+
 class Hashable:
 
     ATTRIBUTES_TO_HASH = None
@@ -19,40 +28,35 @@ class Hashable:
 
             attribute = getattr(self, attribute_name)
 
-            if isinstance(attribute, str):
-                items_to_hash = attribute
+            try:
+                items_to_hash = _HASH_FUNCTIONS[type(attribute)](attribute)
 
-            elif isinstance(attribute, int) or isinstance(attribute, float):
-                items_to_hash = str(attribute)
-
-            elif attribute is None:
-                items_to_hash = "None"
-
-            elif isinstance(attribute, dict):
-                items_to_hash = str(sorted(attribute.items()))
-
-            elif isinstance(attribute, collections.abc.Iterable):
-
-                items = tuple(attribute)
-
-                if any(hasattr(item, "blake3_hash") for item in items):
-                    if all(hasattr(item, "blake3_hash") for item in items):
-                        items_to_hash = str(sorted(subitem.blake3_hash for subitem in items))
-                    else:
-                        raise ValueError(f"Mixed types in attribute {attribute_name!r}: {attribute!r}")
-
+            except KeyError:
+                if isinstance(attribute, collections.abc.Iterable):
+                    items_to_hash = self._hash_iterable(attribute_name, attribute)
                 else:
-                    try:
-                        items_to_hash = str(sorted(items))
-                    except TypeError:
-                        raise TypeError(
-                            f"Attribute needs to be sorted for consistent hash output, but cannot be: "
-                            f"{attribute_name!r}: {attribute!r}"
-                        )
-
-            else:
-                raise TypeError(f"Attribute {attribute_name!r}: {attribute!r} cannot be hashed.")
+                    raise TypeError(f"Attribute <{attribute_name!r}: {attribute!r}> cannot be hashed.")
 
             blake3_hash.update(items_to_hash.encode())
 
         return blake3_hash.hexdigest()
+
+    @staticmethod
+    def _hash_iterable(attribute_name, attribute):
+        items = tuple(attribute)
+
+        if any(hasattr(item, "blake3_hash") for item in items):
+
+            if not all(hasattr(item, "blake3_hash") for item in items):
+                raise ValueError(f"Mixed types in attribute <{attribute_name!r}: {attribute!r}>")
+
+            return str(sorted(subitem.blake3_hash for subitem in items))
+
+        try:
+            return str(sorted(items))
+
+        except TypeError:
+            raise TypeError(
+                f"Attribute needs to be sorted for consistent hash output, but cannot be: "
+                f"<{attribute_name!r}: {attribute!r}>"
+            )
