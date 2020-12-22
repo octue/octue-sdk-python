e9c9493f0aaa2c749a6ba8721a5033ffcf14b9b2
cortadocodes <cortado.codes@protonmail.com>
2020-12-22 17:47:42 +0000

    REF: Rename methods

--- octue/mixins/hashable.py
+++ octue/mixins/hashable.py
@@ -3,7 +3,7 @@ import functools
 from blake3 import blake3
 
 
-_HASH_FUNCTIONS = {
+_HASH_PREPARATION_FUNCTIONS = {
     str: lambda attribute: attribute,
     int: str,
     float: str,
@@ -18,6 +18,8 @@ class Hashable:
 
     @classmethod
     def hash_non_class_object(cls, object_):
+        """ Use the Hashable class to hash an arbitrary object that isn't an attribute of a class instance. """
+
         class Holder(cls):
             ATTRIBUTES_TO_HASH = ("object",)
 
@@ -28,24 +30,25 @@ class Hashable:
     @property
     @functools.lru_cache(maxsize=1)
     def blake3_hash(self):
+        """ Get the BLAKE3 hash of the instance. """
         if not self.ATTRIBUTES_TO_HASH:
             return None
 
         return self._calculate_blake3_hash()
 
     def _calculate_blake3_hash(self, blake3_hash=None):
+        """ Calculate the BLAKE3 hash of the sorted attributes in self.ATTRIBUTES_TO_HASH. """
         blake3_hash = blake3_hash or blake3()
 
         for attribute_name in sorted(self.ATTRIBUTES_TO_HASH):
-
             attribute = getattr(self, attribute_name)
 
             try:
-                items_to_hash = _HASH_FUNCTIONS[type(attribute)](attribute)
+                items_to_hash = _HASH_PREPARATION_FUNCTIONS[type(attribute)](attribute)
 
             except KeyError:
                 if isinstance(attribute, collections.abc.Iterable):
-                    items_to_hash = self._hash_iterable(attribute_name, attribute)
+                    items_to_hash = self._prepare_iterable_for_hashing(attribute_name, attribute)
                 else:
                     raise TypeError(f"Attribute <{attribute_name!r}: {attribute!r}> cannot be hashed.")
 
@@ -54,7 +57,8 @@ class Hashable:
         return blake3_hash.hexdigest()
 
     @staticmethod
-    def _hash_iterable(attribute_name, attribute):
+    def _prepare_iterable_for_hashing(attribute_name, attribute):
+        """ Prepare an iterable attribute for hashing, using the items` own BLAKE3 hashes if available. """
         items = tuple(attribute)
 
         if any(hasattr(item, "blake3_hash") for item in items):
