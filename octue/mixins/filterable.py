8b418a201d1ade250f1eb443d9db7fd69d6e40b2
cortadocodes <cortado.codes@protonmail.com>
2020-12-23 15:24:18 +0000

    FIX: Restore accidentally removed and added files

--- octue/mixins/filterable.py
+++
@@ -1,6 +0,0 @@
-
-
-
-class Filterable:
-
-    pass
---
+++ octue/resources/__init__.py
@@ -0,0 +1,7 @@
+from .analysis import Analysis
+from .datafile import Datafile
+from .dataset import Dataset
+from .manifest import Manifest
+
+
+__all__ = "Analysis", "Datafile", "Dataset", "Manifest"
