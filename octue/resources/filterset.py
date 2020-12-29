8a5c662e5f154438ebefc696d22daee5ad9b3c4a
cortadocodes <cortado.codes@protonmail.com>
2020-12-29 18:04:01 +0000

    IMP: Add FilterList class

--- octue/resources/filterset.py
+++ octue/resources/filterset.py
@@ -1,20 +1,29 @@
-class FilterSet(set):
-    def filter(self, filter_name=None, filter_value=None):
-        """ Returns a new FilterSet containing only the Filterees to which the given filter criteria apply.
-
-        Say we want to filter by files whose extension equals "csv". We want to be able to do...
-
-        ds = DataSet(... initialise with csv files and other files)
-        Example of chaining:
-
-        ds.filter('files__extension_equals', 'csv')
-        is equvalent to
-        ds.files.filter(extension__equals', 'csv')
-        >>> FilterSet containing a set of datafiles
+BASE_TYPES = (set, list)
 
 
-        ds.filter('files__extension_equals', 'csv')
-        is equvalent to
-        ds.files.filter(extension__equals', 'csv')
-        """
-        return FilterSet((item for item in self if item.check_attribute(filter_name, filter_value)))
+def _filter(instance, filter_name=None, filter_value=None):
+    """ Returns a new FilterSet containing only the Filterees to which the given filter criteria apply.
+
+    Say we want to filter by files whose extension equals "csv". We want to be able to do...
+
+    ds = DataSet(... initialise with csv files and other files)
+    Example of chaining:
+
+    ds.filter('files__extension_equals', 'csv')
+    is equvalent to
+    ds.files.filter(extension__equals', 'csv')
+    >>> FilterSet containing a set of datafiles
+
+
+    ds.filter('files__extension_equals', 'csv')
+    is equvalent to
+    ds.files.filter(extension__equals', 'csv')
+    """
+    return instance.__class__((item for item in instance if item.check_attribute(filter_name, filter_value)))
+
+
+# This defines FilterSet and FilterList classes that inherit from `set` and `list` respectively while implementing a
+# `filter` method (i.e. `_filter` above).
+for type_ in BASE_TYPES:
+    class_name = f"Filter{type_.__name__.title()}"
+    globals()[class_name] = type(class_name, (type_,), {"filter": _filter})
