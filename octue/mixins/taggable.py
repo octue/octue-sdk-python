133aa47fbfc8956ec50eed2d1a7bdda473f10be4
cortadocodes <cortado.codes@protonmail.com>
2020-12-30 12:07:12 +0000

    REF: Move Tag and TagGroup into resources package

--- octue/mixins/taggable.py
+++ octue/mixins/taggable.py
@@ -1,197 +1,8 @@
-import re
-from functools import lru_cache
-
-from octue.exceptions import InvalidTagException
-from octue.mixins import Filteree
-from octue.resources.filter_containers import FilterSet
-
-
-# A tag starts and ends with a character in [a-z] or [0-9]. It can contain the colon discriminator or hyphens.
-# Empty strings are also valid.
-# Valid examples:
-#    system:32
-#    angry-marmaduke
-#    mega-man:torso:component:12
-TAG_PATTERN = re.compile(r"^$|^[a-z0-9][a-z0-9:\-]*(?<![:-])$")
-
-
-class Tag(Filteree):
-
-    _FILTERABLE_ATTRIBUTES = ("name",)
-
-    def __init__(self, name):
-        self._name = self._clean(name)
-
-    @property
-    def name(self):
-        return self._name
-
-    @property
-    @lru_cache(maxsize=1)
-    def subtags(self):
-        return FilterSet({Tag(subtag_name) for subtag_name in (self.name.split(":"))})
-
-    def __eq__(self, other):
-        if isinstance(other, str):
-            return self.name == Tag(other).name
-        elif isinstance(other, Tag):
-            return self.name == other.name
-        return False
-
-    def __lt__(self, other):
-        if isinstance(other, str):
-            return self.name < Tag(other).name
-        elif isinstance(other, Tag):
-            return self.name < other.name
-        return False
-
-    def __gt__(self, other):
-        if isinstance(other, str):
-            return self.name > Tag(other).name
-        elif isinstance(other, Tag):
-            return self.name > other.name
-        return False
-
-    def __hash__(self):
-        return hash(f"{type(self).__name__}{self.name}")
-
-    def __contains__(self, item):
-        return item in self.name
-
-    def __repr__(self):
-        return repr(self.name)
-
-    def starts_with(self, value, consider_separate_subtags=False):
-        """ Implement a startswith method that returns true if any of the tags starts with value """
-        if not consider_separate_subtags:
-            return self.name.startswith(value)
-
-        return any(subtag.startswith(value) for subtag in self.subtags)
-
-    def ends_with(self, value, consider_separate_subtags=False):
-        """ Implement an endswith method that returns true if any of the tags endswith value. """
-        if not consider_separate_subtags:
-            return self.name.endswith(value)
-
-        return any(subtag.ends_with(value) for subtag in self.subtags)
-
-    def contains(self, value):
-        """ Implement a contains method that returns true if any of the tags contains value. """
-        return value in self.name
-
-    @staticmethod
-    def _clean(name):
-        """ Private method to clean up an iterable of tags into a list of cleaned tags
-        """
-        # Check they're strings
-        if not isinstance(name, str):
-            raise InvalidTagException("Tags must be expressed as a string")
-
-        # Strip leading and trailing whitespace, ensure they match the regex
-        cleaned_name = name.strip()
-
-        if not re.match(TAG_PATTERN, cleaned_name):
-            raise InvalidTagException(
-                f"Invalid tag '{cleaned_name}'. Tags must contain only characters 'a-z', '0-9', ':' and '-'. They must "
-                f"not start with '-' or ':'."
-            )
-
-        return cleaned_name
-
-
-class TagGroup:
-    """ Class to handle a group of tags as a string.
-    """
-
-    _FILTERSET_ATTRIBUTE = "tags"
-
-    def __init__(self, tags, *args, **kwargs):
-        """ Construct a TagGroup
-        """
-        # TODO Call the superclass with *args anad **kwargs, then update everything to using ResourceBase
-        if tags is None:
-            self.tags = FilterSet()
-
-        elif isinstance(tags, str):
-            self.tags = FilterSet(Tag(tag) for tag in tags.strip().split())
-
-        elif isinstance(tags, TagGroup):
-            self.tags = FilterSet(tags.tags)
-
-        # Tags can be some other iterable than a list, but each tag must be a string
-        elif hasattr(tags, "__iter__"):
-            self.tags = FilterSet(tags)
-
-        else:
-            raise InvalidTagException(
-                "Tags must be expressed as a whitespace-delimited string or an iterable of strings or Tag instances."
-            )
-
-    def __str__(self):
-        """ Serialise tags to a sorted list string. """
-        return self.serialise()
-
-    def __eq__(self, other):
-        """ Does this TagGroup have the same tags as another TagGroup? """
-        if not isinstance(other, TagGroup):
-            return False
-        return self.tags == other.tags
-
-    def __iter__(self):
-        """ Iterate over the tags in the TagGroup. """
-        yield from self.tags
-
-    def __len__(self):
-        return len(self.tags)
-
-    def __repr__(self):
-        return f"<TagGroup({self.tags})>"
-
-    def _yield_subtags(self):
-        """ Yield the colon-separated subtags of a tag as strings, including the main tag. """
-        for tag in self.tags:
-            yield from tag.subtags
-
-    def add_tags(self, *args):
-        """ Adds one or more new tag strings to the object tags. New tags will be cleaned and validated.
-        """
-        self.tags |= {Tag(arg) for arg in args}
-
-    def has_tag(self, tag):
-        """ Returns true if any of the tags exactly matches value, allowing test like `if 'a' in TagGroup('a b')`
-        """
-        return tag in self.tags or Tag(tag) in self.tags
-
-    def get_subtags(self):
-        """ Return a new TagGroup instance with all the subtags. """
-        return TagGroup(FilterSet(self._yield_subtags()))
-
-    def starts_with(self, value, consider_separate_subtags=False):
-        """ Implement a startswith method that returns true if any of the tags starts with value """
-        if not consider_separate_subtags:
-            return any(tag.starts_with(value) for tag in self.tags)
-
-        return any(subtag.starts_with(value) for subtag in self._yield_subtags())
-
-    def ends_with(self, value, consider_separate_subtags=False):
-        """ Implement an endswith method that returns true if any of the tags endswith value. """
-        if not consider_separate_subtags:
-            return any(tag.ends_with(value) for tag in self.tags)
-
-        return any(subtag.ends_with(value) for subtag in self._yield_subtags())
-
-    def any_tag_contains(self, value):
-        """ Implement a contains method that returns true if any of the tags contains value. """
-        return any(value in tag for tag in self.tags)
-
-    def serialise(self):
-        """ Serialise tags to a sorted list string. """
-        return str(sorted(self))
+from octue.resources.tag import TagGroup
 
 
 class Taggable:
-    """ A mixin class allowing objects to be tagged
-    """
+    """ A mixin class allowing objects to be tagged. """
 
     def __init__(self, *args, tags=None, **kwargs):
         """ Constructor for Taggable mixins
@@ -200,8 +11,7 @@ class Taggable:
         self._tags = TagGroup(tags)
 
     def add_tags(self, *args):
-        """ Adds one or more new tag strings to the object tags. New tags will be cleaned and validated.
-        """
+        """ Adds one or more new tag strings to the object tags. New tags will be cleaned and validated. """
         self._tags.add_tags(*args)
 
     @property
@@ -210,6 +20,5 @@ class Taggable:
 
     @tags.setter
     def tags(self, tags):
-        """ Overwrite any existing tag group and assign new tags
-        """
+        """ Overwrite any existing tag group and assign new tags. """
         self._tags = TagGroup(tags)
---
+++ tests/resources/test_tag.py
@@ -0,0 +1,110 @@
+from tests.base import BaseTestCase
+
+from octue.resources.filter_containers import FilterSet
+from octue.resources.tag import Tag, TagGroup
+
+
+class TestTagGroup(BaseTestCase):
+    TAG_GROUP = TagGroup(tags="a b:c d:e:f")
+
+    def test_equality(self):
+        """ Ensure two TagGroups with the same tags compare equal. """
+        self.assertTrue(self.TAG_GROUP == TagGroup(tags="a b:c d:e:f"))
+
+    def test_inequality(self):
+        """ Ensure two TagGroups with different tags compare unequal. """
+        self.assertTrue(self.TAG_GROUP != TagGroup(tags="a"))
+
+    def test_iterating_over(self):
+        """ Ensure a TagGroup can be iterated over. """
+        self.assertEqual(set(self.TAG_GROUP), {Tag("a"), Tag("b:c"), Tag("d:e:f")})
+
+    def test_has_tag(self):
+        """ Ensure we can check that a TagGroup has a certain tag. """
+        self.assertTrue(self.TAG_GROUP.has_tag("d:e:f"))
+
+    def test_does_not_have_tag(self):
+        """ Ensure we can check that a TagGroup does not have a certain tag. """
+        self.assertFalse(self.TAG_GROUP.has_tag("hello"))
+
+    def test_has_tag_only_matches_full_tags(self):
+        """ Test that the has_tag method only matches full tags (i.e. that it doesn't match subtags or parts of tags."""
+        for tag in "a", "b:c", "d:e:f":
+            self.assertTrue(self.TAG_GROUP.has_tag(tag))
+
+        for tag in "b", "c", "d", "e", "f":
+            self.assertFalse(self.TAG_GROUP.has_tag(tag))
+
+    def test_yield_subtags(self):
+        """ Test that subtags can be yielded from tags, including the main tags themselves. """
+        self.assertEqual(
+            set(self.TAG_GROUP._yield_subtags()), {Tag("a"), Tag("b"), Tag("c"), Tag("d"), Tag("e"), Tag("f")}
+        )
+
+    def test_get_subtags(self):
+        """ Test subtags can be accessed as a new TagGroup. """
+        self.assertEqual(TagGroup("meta:sys2:3456 blah").get_subtags(), TagGroup("meta sys2 3456 blah"))
+
+    def test_starts_with_with_no_subtags(self):
+        """ Ensure starts_with doesn't check starts of subtags by default. """
+        for tag in "a", "b", "d":
+            self.assertTrue(self.TAG_GROUP.starts_with(tag))
+
+        for tag in "c", "e", "f":
+            self.assertFalse(self.TAG_GROUP.starts_with(tag))
+
+    def test_starts_with_with_subtags(self):
+        """ Ensure starts_with checks starts of subtags if asked. """
+        for tag in "a", "b", "c", "d", "e", "f":
+            self.assertTrue(self.TAG_GROUP.starts_with(tag, consider_separate_subtags=True))
+
+    def test_endsswith_with_no_subtags(self):
+        """ Ensure ends_with doesn't check ends of subtags by default. """
+        for tag in "a", "c", "f":
+            self.assertTrue(self.TAG_GROUP.ends_with(tag))
+
+        for tag in "b", "d", "e":
+            self.assertFalse(self.TAG_GROUP.ends_with(tag))
+
+    def test_ends_with_with_subtags(self):
+        """ Ensure ends_with checks ends of subtags if asked. """
+        for tag in "a", "b", "c", "d", "e", "f":
+            self.assertTrue(self.TAG_GROUP.ends_with(tag, consider_separate_subtags=True))
+
+    def test_contains_searches_for_tags_and_subtags(self):
+        """ Ensure tags and subtags can be searched for. """
+        for tag in "a", "b", "d":
+            self.assertTrue(self.TAG_GROUP.any_tag_contains(tag))
+
+        for subtag in "c", "e", "f":
+            self.assertTrue(self.TAG_GROUP.any_tag_contains(subtag))
+
+    def test_filter(self):
+        """ Test that tag groups can be filtered. """
+        tag_group = TagGroup(tags="tag1 tag2 meta:sys1:1234 meta:sys2:3456 meta:sys2:55")
+        self.assertEqual(
+            tag_group.tags.filter("name__starts_with", "meta"),
+            FilterSet({Tag("meta:sys1:1234"), Tag("meta:sys2:3456"), Tag("meta:sys2:55")}),
+        )
+
+    def test_filter_chaining(self):
+        """ Test that filters can be chained. """
+        tag_group = TagGroup(tags="tag1 tag2 meta:sys1:1234 meta:sys2:3456 meta:sys2:55")
+
+        filtered_tags_1 = tag_group.tags.filter("name__starts_with", "meta")
+        self.assertEqual(filtered_tags_1, TagGroup("meta:sys1:1234 meta:sys2:3456 meta:sys2:55").tags)
+
+        filtered_tags_2 = filtered_tags_1.filter("name__contains", "sys2")
+        self.assertEqual(filtered_tags_2, TagGroup("meta:sys2:3456 meta:sys2:55").tags)
+
+        filtered_tags_3 = filtered_tags_1.filter("name__equals", "meta:sys2:55")
+        self.assertEqual(filtered_tags_3, TagGroup("meta:sys2:55").tags)
+
+    def test_serialise(self):
+        """ Ensure that TagGroups are serialised to the string form of a list. """
+        self.assertEqual(self.TAG_GROUP.serialise(), "['a', 'b:c', 'd:e:f']")
+
+    def test_serialise_orders_tags(self):
+        """ Ensure that TagGroups are serialised to the string form of a list. """
+        tag_group = TagGroup("z hello a c:no")
+        self.assertEqual(tag_group.serialise(), "['a', 'c:no', 'hello', 'z']")
