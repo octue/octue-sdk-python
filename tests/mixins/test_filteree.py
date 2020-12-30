67b80fd6b258e117448a318f62d3a45ab61f9dfb
cortadocodes <cortado.codes@protonmail.com>
2020-12-30 14:51:02 +0000

    TST: Deepen Filtree tests and reduce code

--- tests/mixins/test_filteree.py
+++ tests/mixins/test_filteree.py
@@ -5,8 +5,14 @@ from octue.mixins.filteree import Filteree
 
 
 class FiltereeSubclass(Filteree):
-    _FILTERABLE_ATTRIBUTES = ("cat",)
-    cat = "cat"
+    _FILTERABLE_ATTRIBUTES = ("name", "is_alive", "iterable", "age", "owner")
+
+    def __init__(self, name=None, is_alive=None, iterable=None, age=None, owner=None):
+        self.name = name
+        self.is_alive = is_alive
+        self.iterable = iterable
+        self.age = age
+        self.owner = owner
 
 
 class TestFilteree(BaseTestCase):
@@ -14,8 +20,11 @@ class TestFilteree(BaseTestCase):
         """ Ensure an error is raised if attempting to check attributes when no filterable attributes have been defined
         in the Filteree class.
         """
+        filteree = Filteree()
+        filteree.name = "Fred"
+
         with self.assertRaises(ValueError):
-            Filteree().satisfies(None, None)
+            filteree.satisfies("name__contains", "F")
 
     def test_error_raised_when_invalid_filter_name_received(self):
         """ Ensure an error is raised when an invalid filter name is provided. """
@@ -25,30 +34,20 @@ class TestFilteree(BaseTestCase):
     def test_error_raised_when_valid_but_non_existent_filter_name_received(self):
         """ Ensure an error is raised when a valid but non-existent filter name is received. """
         with self.assertRaises(exceptions.InvalidInputException):
-            FiltereeSubclass().satisfies(filter_name="cat__out_of_the_bag", filter_value=True)
+            FiltereeSubclass().satisfies(filter_name="age__is_secret", filter_value=True)
 
     def test_error_raised_when_attribute_type_has_no_filters_defined(self):
         """ Ensure an error is raised when a filter for an attribute whose type doesn't have any filters defined is
         received.
         """
-        filteree_subclass = FiltereeSubclass()
-        filteree_subclass.cat = lambda: None
-
         with self.assertRaises(exceptions.InvalidInputException):
-            filteree_subclass.satisfies(filter_name="cat__contains", filter_value=True)
+            FiltereeSubclass(age=lambda: None).satisfies(filter_name="age__equals", filter_value=True)
 
     def test_check_attribute(self):
         """ Ensure filterable attributes can be checked for filter satisfaction. """
-
-        class MyClass(Filteree):
-            _FILTERABLE_ATTRIBUTES = ("name", "is_alive", "iterable")
-
-            def __init__(self, name, is_alive, iterable):
-                self.name = name
-                self.is_alive = is_alive
-                self.iterable = iterable
-
-        filterable_thing = MyClass(name="Fred", is_alive=True, iterable={1, 2, 3})
+        filterable_thing = FiltereeSubclass(name="Fred", is_alive=True, iterable={1, 2, 3}, age=5.2, owner=None)
         self.assertTrue(filterable_thing.satisfies("name__icontains", "f"))
         self.assertFalse(filterable_thing.satisfies("is_alive__is", False))
         self.assertTrue(filterable_thing.satisfies("iterable__contains", 3))
+        self.assertTrue(filterable_thing.satisfies("age__equals", 5.2))
+        self.assertTrue(filterable_thing.satisfies("owner__is_none", True))
