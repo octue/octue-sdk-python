f3e45f849ad5ed017a628707956d48298c415a2f
cortadocodes <cortado.codes@protonmail.com>
2021-01-04 15:05:13 +0000

    TST: Test ordering by list attributes

--- tests/resources/test_filter_containers.py
+++ tests/resources/test_filter_containers.py
@@ -15,22 +15,26 @@ class Cat(Filterable):
 
 
 class TestFilterSet(BaseTestCase):
-    def test_order_by_with_string_attribute(self):
-        """ Test ordering a FilterSet by a string attribute. """
-        cats = [Cat(name="Zorg"), Cat(name="James"), Cat(name="Princess Carolyne")]
-        filter_set = FilterSet(cats)
-        sorted_filter_set = filter_set.order_by("name")
-        self.assertEqual(sorted_filter_set, FilterList([cats[1], cats[2], cats[0]]))
-
-    def test_order_by_with_int_attribute(self):
-        """ Test ordering a FilterSet by an integer attribute. """
-        cats = [Cat(age=5), Cat(age=4), Cat(age=3)]
-        filter_set = FilterSet(cats)
-        sorted_filter_set = filter_set.order_by("age")
-        self.assertEqual(sorted_filter_set, FilterList([cats[2], cats[1], cats[0]]))
-
     def test_ordering_by_a_non_existent_attribute(self):
         """ Ensure an error is raised if ordering is attempted by a non-existent attribute. """
         filter_set = FilterSet([Cat(age=5), Cat(age=4), Cat(age=3)])
         with self.assertRaises(exceptions.InvalidInputException):
             filter_set.order_by("dog-likeness")
+
+    def test_order_by_with_string_attribute(self):
+        """ Test ordering a FilterSet by a string attribute returns an appropriately ordered FilterList. """
+        cats = [Cat(name="Zorg"), Cat(name="James"), Cat(name="Princess Carolyn")]
+        sorted_filter_set = FilterSet(cats).order_by("name")
+        self.assertEqual(sorted_filter_set, FilterList([cats[1], cats[2], cats[0]]))
+
+    def test_order_by_with_int_attribute(self):
+        """ Test ordering a FilterSet by an integer attribute returns an appropriately ordered FilterList. """
+        cats = [Cat(age=5), Cat(age=4), Cat(age=3)]
+        sorted_filter_set = FilterSet(cats).order_by("age")
+        self.assertEqual(sorted_filter_set, FilterList(reversed(cats)))
+
+    def test_order_by_iterable(self):
+        """ Test that ordering by list attributes orders by the size of the list. """
+        cats = [Cat(previous_names=["Scatta", "Catta"]), Cat(previous_names=["Kitty"]), Cat(previous_names=[])]
+        sorted_filter_set = FilterSet(cats).order_by("previous_names")
+        self.assertEqual(sorted_filter_set, FilterList(reversed(cats)))
