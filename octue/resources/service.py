ff9c5aa72196be8943e51850e8c85985a81d4d88
cortadocodes <cortado.codes@protonmail.com>
2021-01-06 16:14:45 +0000

    IMP: Add representation and ask method to Service

--- octue/resources/service.py
+++ octue/resources/service.py
@@ -3,3 +3,9 @@ class Service:
         self.name = name
         self.id = id
         self.uri = uri
+
+    def __repr(self):
+        return f"<{type(self).__name__}({self.name!r})>"
+
+    def ask(self, input_values, input_manifest=None):
+        pass
