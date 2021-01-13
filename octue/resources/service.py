ddfd2f6cca65a20cb32a51aef0d5d94ccb91a32f
cortadocodes <cortado.codes@protonmail.com>
2021-01-13 20:17:04 +0000

    FIX: Properly encode and decode pub/sub data

--- octue/resources/service.py
+++ octue/resources/service.py
@@ -1,3 +1,4 @@
+import json
 import logging
 from concurrent.futures import TimeoutError
 import google.api_core.exceptions
@@ -14,7 +15,7 @@ class PublisherSubscriber:
     def __init__(self):
         self._publisher = pubsub_v1.PublisherClient()
         self._subscriber = pubsub_v1.SubscriberClient()
-        self._topic_path = self._publisher.topic_path(GCP_PROJECT, "test_topic")  # TODO: Replace topic name with a UUID
+        self._topic_path = self._publisher.topic_path(GCP_PROJECT, "test-topic")  # TODO: Replace topic name with a UUID
         self._subscription_path = self._subscriber.subscription_path(
             GCP_PROJECT, "test-subscription"
         )  # TODO: Replace subscription name with a UUID
@@ -38,30 +39,29 @@ class PublisherSubscriber:
 
 
 class Service(PublisherSubscriber):
-    def __init__(self, name, id, uri):
+    def __init__(self, name):
         self.name = name
-        self.id = id
-        self.uri = uri
-        self._response = None
         super().__init__()
 
     def __repr__(self):
         return f"<{type(self).__name__}({self.name!r})>"
 
     def ask(self, input_values, input_manifest=None):
-        self._publisher.publish(self._topic_path, b"question", input_values=input_values, input_manifest=input_manifest)
+        self._publisher.publish(self._topic_path, json.dumps(input_values).encode())
         streaming_pull_future = self._subscriber.subscribe(self._subscription_path, callback=self._callback)
 
         with self._subscriber:
             try:
-                streaming_pull_future.result(timeout=5)
+                streaming_pull_future.result(timeout=10)
             except TimeoutError:
                 streaming_pull_future.cancel()
 
-        return vars(self).pop("_response")
+        response = vars(self).pop("_response").data
+        response = json.loads(response.decode())
+        return response
 
     def respond(self, output_values):
-        self._publisher.publish(self._topic_path, b"answer", output_values=output_values)
+        self._publisher.publish(self._topic_path, json.dumps(output_values).encode())
 
     def _callback(self, response):
         self._response = response
