f781ffeacfd8330cc4321a80e0d32c0043511b14
cortadocodes <cortado.codes@protonmail.com>
2021-01-18 12:12:44 +0000

    TST: Test PublisherSubscriber

--- tests/resources/test_service.py
+++ tests/resources/test_service.py
@@ -1,7 +1,35 @@
 import time
 from tests.base import BaseTestCase
 
-from octue.resources.service import Service
+from octue.resources.service import GCP_PROJECT, PublisherSubscriber, Service
+
+
+class TestPublisherSubscriber(BaseTestCase):
+    pub_sub = PublisherSubscriber()
+
+    def test_initialise_topic(self):
+        """ Test a topic can be initialised. """
+        topic = self.pub_sub._initialise_topic("blah")
+        self.assertEqual(topic, "/".join(("projects", GCP_PROJECT, "topics", "blah")))
+
+    def test_initialise_existing_topic(self):
+        """ Test that initialising an existing topic returns the existing topic. """
+        topic_1 = self.pub_sub._initialise_topic("blah")
+        topic_2 = self.pub_sub._initialise_topic("blah")
+        self.assertEqual(topic_1, topic_2)
+
+    def test_initialise_subscription(self):
+        """ Test a subscription can be initialised. """
+        topic = self.pub_sub._initialise_topic("blah")
+        subscription = self.pub_sub._initialise_subscription(topic, "blah-sub")
+        self.assertEqual(subscription, "/".join(("projects", GCP_PROJECT, "subscriptions", "blah-sub")))
+
+    def test_initialise_existing_subscription(self):
+        """ Test that initialising an existing subscription returns the existing subscription. """
+        topic = self.pub_sub._initialise_topic("blah")
+        subscription_1 = self.pub_sub._initialise_subscription(topic, "blah-sub")
+        subscription_2 = self.pub_sub._initialise_subscription(topic, "blah-sub")
+        self.assertEqual(subscription_1, subscription_2)
 
 
 class TestService(BaseTestCase):
