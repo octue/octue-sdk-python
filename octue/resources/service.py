d249d128a0bd9ef0bfb8b49d52b572c02f1f9360
cortadocodes <cortado.codes@protonmail.com>
2021-01-20 20:26:57 +0000

    FIX: Use one pub/sub per Service; remove Topic and Subscription context managers

--- octue/resources/service.py
+++ octue/resources/service.py
@@ -12,23 +12,16 @@ logger = logging.getLogger(__name__)
 
 OCTUE_NAMESPACE = "octue.services"
 
+# Switch message batching off by setting max_messages to 1. This minimises latency and is recommended for
+# microservices publishing single messages in a request-response sequence.
+BATCH_SETTINGS = pubsub_v1.types.BatchSettings(max_bytes=10 * 1000 * 1000, max_latency=0.01, max_messages=1)
+
 
 class Topic:
-
-    # Switch message batching off by setting max_messages to 1. This minimises latency and is recommended for
-    # microservices publishing single messages in a request-response sequence.
-    BATCH_SETTINGS = pubsub_v1.types.BatchSettings(max_bytes=10 * 1000 * 1000, max_latency=0.01, max_messages=1)
-
-    def __init__(self, name, gcp_project_name):
+    def __init__(self, name, gcp_project_name, publisher):
         self.name = name
-        self._publisher = pubsub_v1.PublisherClient(self.BATCH_SETTINGS)
+        self._publisher = publisher
         self.path = self._publisher.topic_path(gcp_project_name, f"{OCTUE_NAMESPACE}.{self.name}")
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.delete()
 
     def create(self, allow_existing=False):
         if not allow_existing:
@@ -42,24 +35,13 @@ class Topic:
             except google.api_core.exceptions.AlreadyExists:
                 pass
 
-    def delete(self):
-        self._publisher.stop()
-        self._publisher.delete_topic(topic=self.path)
-        logger.debug("Deleted topic %r and stopped publisher.", self.path)
-
 
 class Subscription:
-    def __init__(self, name, topic, gcp_project_name):
+    def __init__(self, name, topic, gcp_project_name, subscriber):
         self.name = name
         self.topic = topic
-        self.subscriber = pubsub_v1.SubscriberClient()
+        self.subscriber = subscriber
         self.path = self.subscriber.subscription_path(gcp_project_name, f"{OCTUE_NAMESPACE}.{self.name}")
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.delete()
 
     def create(self, allow_existing=False):
         if not allow_existing:
@@ -73,36 +55,37 @@ class Subscription:
             except google.api_core.exceptions.AlreadyExists:
                 pass
 
-    def delete(self):
-        self.subscriber.delete_subscription(subscription=self.path)
-        self.subscriber.close()
-        logger.debug("Deleted subscription %r and closed subscriber.", self.path)
-
 
 class Service:
     def __init__(self, name, gcp_project_name, run_function=None):
         self.name = name
         self.gcp_project_name = gcp_project_name
         self.run_function = run_function
-        self._publisher = pubsub_v1.PublisherClient()
+        self._publisher = pubsub_v1.PublisherClient(BATCH_SETTINGS)
         self._subscriber = pubsub_v1.SubscriberClient()
 
     def __repr__(self):
         return f"<{type(self).__name__}({self.name!r})>"
 
     def serve(self, timeout=None):
-        with Topic(name=self.name, gcp_project_name=self.gcp_project_name) as topic:
-            topic.create()
+        topic = Topic(name=self.name, gcp_project_name=self.gcp_project_name, publisher=self._publisher)
+        topic.create(allow_existing=True)
 
-            with Subscription(name=self.name, topic=topic, gcp_project_name=self.gcp_project_name) as subscription:
-                subscription.create()
-                future = self._subscriber.subscribe(subscription=subscription.path, callback=self.respond)
-                logger.debug("%r server is waiting for questions.", self)
+        subscription = Subscription(
+            name=self.name, topic=topic, gcp_project_name=self.gcp_project_name, subscriber=self._subscriber
+        )
+        subscription.create()
+        future = self._subscriber.subscribe(subscription=subscription.path, callback=self.respond)
+        logger.debug("%r server is waiting for questions.", self)
 
-                try:
-                    future.result(timeout=timeout)
-                except TimeoutError:
-                    future.cancel()
+        with self._subscriber:
+            try:
+                future.result(timeout=timeout)
+            except TimeoutError:
+                future.cancel()
+
+        self._subscriber.delete_subscription(subscription=subscription.path)
+        self._publisher.delete_topic(topic=topic.path)
 
     def respond(self, question):
         logger.info("%r received a question.", self)
@@ -112,7 +95,11 @@ class Service:
 
         output_values = self.run_function(data).output_values
 
-        topic = Topic(name=f"{self.name}.response.{question_uuid}", gcp_project_name=self.gcp_project_name)
+        topic = Topic(
+            name=f"{self.name}.response.{question_uuid}",
+            gcp_project_name=self.gcp_project_name,
+            publisher=self._publisher,
+        )
         self._publisher.publish(topic=topic.path, data=json.dumps(output_values).encode())
         logger.info("%r responded on topic %r.", self, topic.path)
 
@@ -120,15 +107,20 @@ class Service:
         question_uuid = str(int(uuid.uuid4()))
 
         response_topic_and_subscription_name = f"{service_name}.response.{question_uuid}"
-        response_topic = Topic(name=response_topic_and_subscription_name, gcp_project_name=self.gcp_project_name)
+        response_topic = Topic(
+            name=response_topic_and_subscription_name, gcp_project_name=self.gcp_project_name, publisher=self._publisher
+        )
         response_topic.create(allow_existing=False)
 
         response_subscription = Subscription(
-            name=response_topic_and_subscription_name, topic=response_topic, gcp_project_name=self.gcp_project_name
+            name=response_topic_and_subscription_name,
+            topic=response_topic,
+            gcp_project_name=self.gcp_project_name,
+            subscriber=self._subscriber,
         )
         response_subscription.create(allow_existing=False)
 
-        question_topic = Topic(name=service_name, gcp_project_name=self.gcp_project_name)
+        question_topic = Topic(name=service_name, gcp_project_name=self.gcp_project_name, publisher=self._publisher)
         future = self._publisher.publish(
             topic=question_topic.path, data=json.dumps(input_values).encode(), uuid=question_uuid
         )
@@ -145,5 +137,6 @@ class Service:
         self._subscriber.acknowledge(request={"subscription": subscription.path, "ack_ids": [answer.ack_id]})
         logger.debug("%r received a response to question on topic %r", self, subscription.topic)
 
-        subscription.delete()
+        self._subscriber.delete_subscription(subscription=subscription.path)
+        self._publisher.delete_topic(topic=subscription.topic.path)
         return json.loads(answer.message.data.decode())
