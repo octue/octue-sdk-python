7f2cccc6dba568af08076350e4c2ee497734f698
cortadocodes <cortado.codes@protonmail.com>
2021-01-22 18:36:40 +0000

    TST: Test error raised if asking non-existent service question

--- tests/resources/test_service.py
+++ tests/resources/test_service.py
@@ -2,6 +2,7 @@ import concurrent.futures
 import time
 from tests.base import BaseTestCase
 
+from octue import exceptions
 from octue.resources.service import Service
 
 
@@ -16,10 +17,18 @@ class TestService(BaseTestCase):
     GCP_PROJECT = "octue-amy"
 
     def ask_question_and_wait_for_answer(self, asking_service, responding_service, input_values):
-        """ Get an asking service to ask a question to a responding service and wait for the answer."""
+        """ Get an asking service to ask a question to a responding service and wait for the answer. """
         time.sleep(5)  # Wait for the responding service to be ready to answer.
         subscription = asking_service.ask(service_id=responding_service.id, input_values=input_values)
         return asking_service.wait_for_answer(subscription)
+
+    def test_ask_on_non_existent_service_results_in_error(self):
+        """ Test that trying to ask a question to a non-existent service (i.e. one without a topic in Google Pub/Sub)
+        results in an error. """
+        with self.assertRaises(exceptions.ServiceNotFound):
+            Service(name="asker", gcp_project_name=self.GCP_PROJECT, id="249fc09d-9d6f-45d6-b1a4-0aacba5fca79").ask(
+                service_id=1234, input_values=[1, 2, 3, 4]
+            )
 
     def test_ask(self):
         """ Test that a service can ask a question to another service that is serving and receive an answer. """
