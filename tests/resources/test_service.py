33c733e7a26d21434a3325b7cd99f97d78334f10
cortadocodes <cortado.codes@protonmail.com>
2021-01-23 11:55:26 +0000

    TST: Factor out server instantiation

--- tests/resources/test_service.py
+++ tests/resources/test_service.py
@@ -18,11 +18,27 @@ class TestService(BaseTestCase):
     GCP_PROJECT = "octue-amy"
     asking_service = Service(name="asker", gcp_project_name=GCP_PROJECT, id="249fc09d-9d6f-45d6-b1a4-0aacba5fca79")
 
+    def make_new_server(self):
+        return Service(
+            name="server",
+            gcp_project_name=self.GCP_PROJECT,
+            id="352f8185-1d58-4ddf-8faa-2af96147f96f",
+            run_function=lambda input_values, input_manifest: FakeAnalysis(),
+        )
+
     def ask_question_and_wait_for_answer(self, asking_service, responding_service, input_values, input_manifest):
         """ Get an asking service to ask a question to a responding service and wait for the answer. """
         time.sleep(5)  # Wait for the responding service to be ready to answer.
         subscription = asking_service.ask(responding_service.id, input_values, input_manifest)
         return asking_service.wait_for_answer(subscription)
+
+    def test_serve_with_timeout(self):
+        """ Test that a serving service only serves for as long as its timeout. """
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            start_time = time.perf_counter()
+            responding_future = executor.submit(self.make_new_server().serve, timeout=10)
+            list(concurrent.futures.as_completed([responding_future]))[0].result()
+            self.assertTrue(time.perf_counter() - start_time < 15)
 
     def test_ask_on_non_existent_service_results_in_error(self):
         """ Test that trying to ask a question to a non-existent service (i.e. one without a topic in Google Pub/Sub)
@@ -34,12 +50,7 @@ class TestService(BaseTestCase):
 
     def test_ask(self):
         """ Test that a service can ask a question to another service that is serving and receive an answer. """
-        responding_service = Service(
-            name="server",
-            gcp_project_name=self.GCP_PROJECT,
-            id="352f8185-1d58-4ddf-8faa-2af96147f96f",
-            run_function=lambda input_values, input_manifest: FakeAnalysis(),
-        )
+        responding_service = self.make_new_server()
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
             executor.submit(responding_service.serve, timeout=10)
@@ -56,12 +67,10 @@ class TestService(BaseTestCase):
             self.assertEqual(answer, FakeAnalysis.output_values)
 
     def test_ask_with_input_manifest(self):
-        responding_service = Service(
-            name="server",
-            gcp_project_name=self.GCP_PROJECT,
-            id="352f8185-1d58-4ddf-8faa-2af96147f96f",
-            run_function=lambda input_values, input_manifest: FakeAnalysis(),
-        )
+        """ Test that a service can ask a question including an input_manifest to another service that is serving and
+        receive an answer.
+        """
+        responding_service = self.make_new_server()
 
         with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
             executor.submit(responding_service.serve, timeout=10)
