61e4aa5cd924035920e55057e82b543a08240e46
cortadocodes <cortado.codes@protonmail.com>
2021-01-26 11:57:01 +0000

    DOC: Add docstring to backend

--- octue/resources/service_backends.py
+++ octue/resources/service_backends.py
@@ -1,4 +1,6 @@
 class GCPPubSubBackend:
+    """ A dataclass containing the details needed to use Google Cloud Platform Pub/Sub as a Service backend. """
+
     def __init__(self, project_name, credentials_filename):
         self.project_name = project_name
         self.credentials_filename = credentials_filename
