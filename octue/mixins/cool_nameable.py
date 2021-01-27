f950b8e47520e73b6a6da68b2f04fba285ec20f6
cortadocodes <cortado.codes@protonmail.com>
2021-01-27 11:04:06 +0000

    FIX: Stop CoolNameable overriding existing name attribute

--- octue/mixins/cool_nameable.py
+++ octue/mixins/cool_nameable.py
@@ -5,10 +5,10 @@ class CoolNameable:
     """ A mixin that gives the subclass a highly unique human-readable name e.g. "melodic-kestrel". """
 
     def __init__(self, *args, **kwargs):
-        name = kwargs.pop("name", None)
+        existing_name = getattr(self, "name", None) or kwargs.pop("name", None)
 
-        if name is not None:
-            self.name = name
+        if existing_name:
+            self.name = existing_name
             self._cool_named = False
         else:
             self.name = generate_slug(2)
