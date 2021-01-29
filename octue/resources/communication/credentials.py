23cdb17c7cc865459d6aa2ffb3a22e4d5bd8acc2
cortadocodes <cortado.codes@protonmail.com>
2021-01-29 13:41:39 +0000

    REF: Check if envar exists in credentials constructor

--- octue/resources/communication/credentials.py
+++ octue/resources/communication/credentials.py
@@ -15,27 +15,30 @@ class GCPCredentialsManager:
     def __init__(self, environment_variable_name="GCP_SERVICE_ACCOUNT"):
         self.environment_variable_name = environment_variable_name
 
-    def get_credentials(self):
         try:
-            environment_variable_value = os.environ[self.environment_variable_name]
+            self.environment_variable_value = os.environ[self.environment_variable_name]
         except KeyError:
             raise EnvironmentError(f"There is no environment variable called {self.environment_variable_name}.")
 
+    def get_credentials(self):
+        """ Get the Google OAUTH2 service account credentials for which the environment variable value is either the
+        filename containing them or a JSON string version of them. """
+
         # Check that the environment variable refers to a valid *and* real path.
-        if os.path.exists(environment_variable_value):
-            return self._get_credentials_from_file(environment_variable_value)
+        if os.path.exists(self.environment_variable_value):
+            return self._get_credentials_from_file()
 
         # If it doesn't, assume that it's the credentials file as a JSON string.
-        return self._get_credentials_from_string(environment_variable_value)
+        return self._get_credentials_from_string()
 
-    def _get_credentials_from_file(self, environment_variable_value):
-        with open(environment_variable_value) as f:
+    def _get_credentials_from_file(self):
+        with open(self.environment_variable_value) as f:
             credentials = json.load(f)
 
         logger.debug("GCP credentials read from file.")
         return service_account.Credentials.from_service_account_info(credentials)
 
-    def _get_credentials_from_string(self, environment_variable_value):
-        credentials = json.loads(environment_variable_value)
+    def _get_credentials_from_string(self):
+        credentials = json.loads(self.environment_variable_value)
         logger.debug("GCP credentials loaded from string.")
         return service_account.Credentials.from_service_account_info(credentials)
