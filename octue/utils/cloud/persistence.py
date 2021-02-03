3ad9f550245e7717bd2190e6767482e1390d830b
cortadocodes <cortado.codes@protonmail.com>
2021-02-03 17:22:23 +0000

    REF: Make google cloud upload function clearer

--- octue/utils/cloud/persistence.py
+++ octue/utils/cloud/persistence.py
@@ -7,9 +7,9 @@ from octue.utils.cloud.credentials import GCPCredentialsManager
 logger = logging.getLogger(__name__)
 
 
-def upload_file_to_google_cloud(local_path, project_name, bucket_name, remote_path):
+def upload_file_to_google_cloud(local_path, project_name, bucket_name, path_in_bucket):
     """Upload a local file to a Google Cloud bucket at <project_name>/<bucket_name>/<remote_path>."""
     client = storage.Client(project=project_name, credentials=GCPCredentialsManager().get_credentials())
     bucket = client.get_bucket(bucket_or_name=bucket_name)
-    bucket.blob(blob_name=remote_path).upload_from_filename(filename=local_path)
-    logger.info("Uploaded %s to Google cloud at %s/%s/%s.", local_path, project_name, bucket_name, remote_path)
+    bucket.blob(blob_name=path_in_bucket).upload_from_filename(filename=local_path)
+    logger.info("Uploaded %s to Google cloud at gs://%s/%s.", local_path, bucket_name, path_in_bucket)
