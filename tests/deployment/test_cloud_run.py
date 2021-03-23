723793c1af910b4bfb87352c075e9ed513a68755
cortadocodes <cortado.codes@protonmail.com>
2021-03-23 12:54:50 +0000

    TST: Test the cloud run Flask app

--- tests/deployment/test_cloud_run.py
+++ tests/deployment/test_cloud_run.py
@@ -1,8 +1,11 @@
+import base64
 import concurrent.futures
+import json
 import os
 import uuid
 from unittest import TestCase, mock
 
+from octue.deployment.google import cloud_run
 from octue.deployment.google.cloud_run import run_analysis
 from octue.resources.communication.google_pub_sub import Topic
 from octue.resources.communication.google_pub_sub.service import OCTUE_NAMESPACE, Service
@@ -10,7 +13,10 @@ from octue.resources.communication.service_backends import GCPPubSubBackend
 from tests import TEST_PROJECT_NAME, TESTS_DIR
 
 
-class TestRunAnalysis(TestCase):
+cloud_run.app.testing = True
+
+
+class TestCloudRun(TestCase):
     def test_run_analysis(self):
         """Test that the cloud function runs an analysis and publishes the results to Pub/Sub."""
         os.environ["PROJECT_ID"] = TEST_PROJECT_NAME
@@ -51,3 +57,37 @@ class TestRunAnalysis(TestCase):
         answer = answer_future.result()
         self.assertEqual(answer, {"output_values": "It worked!", "output_manifest": None})
         topic.delete()
+
+    def test_post_to_index_with_no_payload_results_in_400_error(self):
+        """Test that a 400 (bad request) error code is returned if no payload is sent to the Flask endpoint."""
+        with cloud_run.app.test_client() as client:
+            response = client.post("/")
+            self.assertEqual(response.status_code, 400)
+
+    def test_post_to_index_with_invalid_payload_results_in_400_error(self):
+        """Test that a 400 (bad request) error code is returned if an invalid payload is sent to the Flask endpoint."""
+        with cloud_run.app.test_client() as client:
+            response = client.post("/", json={"some": "data"})
+            self.assertEqual(response.status_code, 400)
+
+            response = client.post("/", json={"message": "data"})
+            self.assertEqual(response.status_code, 400)
+
+    def test_post_to_index_with_valid_payload(self):
+        """Test that the Flask endpoint returns a 204 (ok, no content) response to a valid payload."""
+        with cloud_run.app.test_client() as client:
+            with mock.patch("octue.deployment.google.cloud_run.run_analysis"):
+
+                response = client.post(
+                    "/",
+                    json={
+                        "message": {
+                            "data": base64.b64encode(
+                                json.dumps({"input_values": [1, 2, 3], "input_manifest": None}).encode()
+                            ).decode(),
+                            "attributes": {"question_uuid": str(uuid.uuid4())},
+                        }
+                    },
+                )
+
+                self.assertEqual(response.status_code, 204)
