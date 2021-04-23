b39b9757db2c51d5c4063a3380b7b82dbd58d3c5
cortadocodes <cortado.codes@protonmail.com>
2021-04-23 17:24:26 +0100

    DEP: Update twined version

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
         "google-cloud-storage>=1.35.1",
         "google-crc32c>=1.1.2",
         "gunicorn",
-        "twined @ https://github.com/octue/twined/archive/release/0.0.18.zip",
+        "twined>=0.0.18",
     ],
     url="https://www.github.com/octue/octue-sdk-python",
     license="MIT",
