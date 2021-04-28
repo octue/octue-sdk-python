86d6eac6387a3bd2bb6e337a153bd8e49c2bb5ce
cortadocodes <cortado.codes@protonmail.com>
2021-04-28 16:04:35 +0100

    TST: Factor topic out of Topic tests

--- tests/cloud/pub_sub/test_topic.py
+++ tests/cloud/pub_sub/test_topic.py
@@ -10,51 +10,45 @@ from tests.base import BaseTestCase
 class TestTopic(BaseTestCase):
 
     service = Service(backend=GCPPubSubBackend(project_name="my-project"))
+    topic = Topic(name="world", namespace="hello", service=service)
 
     def test_namespace_only_in_name_once(self):
         """Test that the topic's namespace only appears in its name once, even if it is repeated."""
-        topic = Topic(name="world", namespace="hello", service=self.service)
-        self.assertEqual(topic.name, "hello.world")
+        self.assertEqual(self.topic.name, "hello.world")
 
         topic_with_repeated_namespace = Topic(name="hello.world", namespace="hello", service=self.service)
         self.assertEqual(topic_with_repeated_namespace.name, "hello.world")
 
     def test_repr(self):
         """Test that Topics are represented correctly."""
-        self.assertEqual(repr(Topic(name="world", namespace="hello", service=self.service)), "<Topic(hello.world)>")
+        self.assertEqual(repr(self.topic), "<Topic(hello.world)>")
 
     def test_error_raised_when_creating_without_allowing_existing_when_topic_already_exists(self):
         """Test that an error is raised when trying to create a topic that already exists and `allow_existing` is
         `False`.
         """
-        topic = Topic(name="world", namespace="hello", service=self.service)
-
         with patch(
             "octue.cloud.pub_sub.service.pubsub_v1.PublisherClient.create_topic",
             side_effect=google.api_core.exceptions.AlreadyExists(""),
         ):
             with self.assertRaises(google.api_core.exceptions.AlreadyExists):
-                topic.create(allow_existing=False)
+                self.topic.create(allow_existing=False)
 
     def test_create_with_allow_existing_when_already_exists(self):
         """Test that trying to create a topic that already exists when `allow_existing` is `True` results in no error."""
-        topic = Topic(name="world", namespace="hello", service=self.service)
-
         with patch(
             "octue.cloud.pub_sub.service.pubsub_v1.PublisherClient.create_topic",
             side_effect=google.api_core.exceptions.AlreadyExists(""),
         ):
-            topic.create(allow_existing=True)
+            self.topic.create(allow_existing=True)
 
     def test_exists(self):
         """Test that topics can be tested for existence."""
-        topic = Topic(name="world", namespace="hello", service=self.service)
-
         with patch("octue.cloud.pub_sub.service.pubsub_v1.PublisherClient.get_topic"):
-            self.assertTrue(topic.exists())
+            self.assertTrue(self.topic.exists())
 
         with patch(
             "octue.cloud.pub_sub.service.pubsub_v1.PublisherClient.get_topic",
             side_effect=google.api_core.exceptions.NotFound(""),
         ):
-            self.assertFalse(topic.exists())
+            self.assertFalse(self.topic.exists())
