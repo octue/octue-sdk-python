05e6d5d801ec15837cd44a277815c56b77a771c1
cortadocodes <cortado.codes@protonmail.com>
2021-05-25 20:37:31 +0100

    REF: Base filter containers on new FilterContainer abstract class

--- octue/resources/filter_containers.py
+++ octue/resources/filter_containers.py
@@ -1,3 +1,4 @@
+from abc import ABC
 from collections import UserDict
 
 from octue import exceptions
@@ -5,12 +6,37 @@ from octue.mixins import Filterable
 from octue.utils.objects import get_nested_attribute
 
 
+class FilterContainer(ABC):
+    def filter(self, ignore_items_without_attribute=True, **kwargs):
+        """Return a new instance of the container containing only the `Filterable`s to which the given filter criteria
+        are `True`.
+
+        :param bool ignore_items_without_attribute:
+        :param {str: any} kwargs: keyword arguments whose keys are the name of the filter and whose values are the
+            values to filter for
+        :return octue.resources.filter_containers.FilterContainer:
+        """
+        pass
+
+    def order_by(self, attribute_name, reverse=False):
+        """Order the `Filterable`s in the container by an attribute with the given name, returning them as a new
+        `FilterList` regardless of the type of filter container begun with.
+
+        :param str attribute_name:
+        :param bool reverse:
+        :raise octue.exceptions.InvalidInputException: if an attribute with the given name doesn't exist on any of the
+            container's members
+        :return FilterList:
+        """
+        pass
+
+
 def _filter(self, ignore_items_without_attribute=True, **kwargs):
     """Return a new instance containing only the Filterables to which the given filter criteria apply.
 
     :param bool ignore_items_without_attribute:
-    :param {str: any} kwargs: a single keyword argument whose key is the name of the filter and whose value is the value
-        to filter for
+    :param {str: any} kwargs: keyword arguments whose keys are the name of the filter and whose values are the
+            values to filter for
     :return octue.resources.filter_containers.FilterSet:
     """
     if any(not isinstance(item, Filterable) for item in self):
@@ -35,8 +61,14 @@ def _filter(self, ignore_items_without_attribute=True, **kwargs):
 
 
 def _order_by(self, attribute_name, reverse=False):
-    """Order the instance by the given attribute_name, returning the instance's elements as a new FilterList (not a
-    FilterSet.
+    """Order the `Filterable`s in the container by an attribute with the given name, returning them as a new
+    `FilterList` regardless of the type of filter container begun with.
+
+    :param str attribute_name:
+    :param bool reverse:
+    :raise octue.exceptions.InvalidInputException: if an attribute with the given name doesn't exist on any of the
+        container's members
+    :return FilterList:
     """
     try:
         return FilterList(sorted(self, key=lambda item: getattr(item, attribute_name), reverse=reverse))
@@ -46,24 +78,24 @@ def _order_by(self, attribute_name, reverse=False):
         )
 
 
-class FilterSet(set):
+class FilterSet(FilterContainer, set):
     filter = _filter
     order_by = _order_by
 
 
-class FilterList(list):
+class FilterList(FilterContainer, list):
     filter = _filter
     order_by = _order_by
 
 
-class FilterDict(UserDict):
+class FilterDict(FilterContainer, UserDict):
     def filter(self, ignore_items_without_attribute=True, **kwargs):
         """Return a new instance containing only the Filterables for which the given filter criteria apply are
         satisfied.
 
         :param bool ignore_items_without_attribute:
-        :param {str: any} kwargs: a single keyword argument whose key is the name of the filter and whose value is the
-            value to filter for
+        :param {str: any} kwargs: keyword arguments whose keys are the name of the filter and whose values are the
+            values to filter for
         :return FilterDict:
         """
         if any(not isinstance(item, Filterable) for item in self.values()):
@@ -87,8 +119,7 @@ class FilterDict(UserDict):
             return self.filter(raise_error_if_filter_is_invalid, **{filter_name: filter_value}).filter(**kwargs)
 
     def order_by(self, attribute_name, reverse=False):
-        """Order the instance by the given attribute_name, returning the instance's elements as a new FilterList (not a
-        FilterSet.
+        """Order the instance by the given attribute_name, returning the instance's elements as a new FilterList.
 
         :param str attribute_name: a dot-separated (optionally nested) attribute name e.g. "a", "a.b", "a.b.c"
         :param bool reverse:
