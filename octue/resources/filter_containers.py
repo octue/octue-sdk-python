6ad724ea526d57bf704126696f2884d8333047d2
cortadocodes <cortado.codes@protonmail.com>
2021-06-02 14:59:57 +0100

    REF: Move filter and order methods into FilterContainer

--- octue/resources/filter_containers.py
+++ octue/resources/filter_containers.py
@@ -8,14 +8,31 @@ from octue.utils.objects import get_nested_attribute
 
 class FilterContainer(ABC):
     def filter(self, ignore_items_without_attribute=True, **kwargs):
-        """Return a new instance of the container containing only the `Filterable`s to which the given filter criteria
-        are `True`.
+        """Return a new instance containing only the `Filterable`s to which the given filter criteria are `True`.
 
         :param bool ignore_items_without_attribute:
-        :param {str: any} kwargs: keyword arguments whose keys are the name of the filter and whose values are the
-            values to filter for
+        :param {str: any} kwargs: keyword arguments whose keys are the name of the filter and whose values are the values to filter for
         :return octue.resources.filter_containers.FilterContainer:
         """
+        if any(not isinstance(item, Filterable) for item in self):
+            raise TypeError(f"All items in a {type(self).__name__} must be of type {Filterable.__name__}.")
+
+        raise_error_if_filter_is_invalid = not ignore_items_without_attribute
+
+        if len(kwargs) == 1:
+            return type(self)(
+                (
+                    item
+                    for item in self
+                    if item.satisfies(raise_error_if_filter_is_invalid=raise_error_if_filter_is_invalid, **kwargs)
+                )
+            )
+
+        filter_names = list(kwargs)
+
+        for filter_name in filter_names:
+            filter_value = kwargs.pop(filter_name)
+            return self.filter(raise_error_if_filter_is_invalid, **{filter_name: filter_value}).filter(**kwargs)
 
     def order_by(self, attribute_name, reverse=False):
         """Order the `Filterable`s in the container by an attribute with the given name, returning them as a new
@@ -23,10 +40,15 @@ class FilterContainer(ABC):
 
         :param str attribute_name:
         :param bool reverse:
-        :raise octue.exceptions.InvalidInputException: if an attribute with the given name doesn't exist on any of the
-            container's members
+        :raise octue.exceptions.InvalidInputException: if an attribute with the given name doesn't exist on any of the container's members
         :return FilterList:
         """
+        try:
+            return FilterList(sorted(self, key=lambda item: getattr(item, attribute_name), reverse=reverse))
+        except AttributeError:
+            raise exceptions.InvalidInputException(
+                f"An attribute named {attribute_name!r} does not exist on one or more members of {self!r}."
+            )
 
     def one(self, **kwargs):
         """If a single result exists for the given filters, return it. Otherwise, raise an error.
@@ -49,61 +71,12 @@ class FilterContainer(ABC):
         return results.pop()
 
 
-def _filter(self, ignore_items_without_attribute=True, **kwargs):
-    """Return a new instance containing only the Filterables to which the given filter criteria apply.
-
-    :param bool ignore_items_without_attribute:
-    :param {str: any} kwargs: keyword arguments whose keys are the name of the filter and whose values are the
-            values to filter for
-    :return octue.resources.filter_containers.FilterSet:
-    """
-    if any(not isinstance(item, Filterable) for item in self):
-        raise TypeError(f"All items in a {type(self).__name__} must be of type {Filterable.__name__}.")
-
-    raise_error_if_filter_is_invalid = not ignore_items_without_attribute
-
-    if len(kwargs) == 1:
-        return type(self)(
-            (
-                item
-                for item in self
-                if item.satisfies(raise_error_if_filter_is_invalid=raise_error_if_filter_is_invalid, **kwargs)
-            )
-        )
-
-    filter_names = list(kwargs)
-
-    for filter_name in filter_names:
-        filter_value = kwargs.pop(filter_name)
-        return _filter(self, raise_error_if_filter_is_invalid, **{filter_name: filter_value}).filter(**kwargs)
-
-
-def _order_by(self, attribute_name, reverse=False):
-    """Order the `Filterable`s in the container by an attribute with the given name, returning them as a new
-    `FilterList` regardless of the type of filter container begun with.
-
-    :param str attribute_name:
-    :param bool reverse:
-    :raise octue.exceptions.InvalidInputException: if an attribute with the given name doesn't exist on any of the
-        container's members
-    :return FilterList:
-    """
-    try:
-        return FilterList(sorted(self, key=lambda item: getattr(item, attribute_name), reverse=reverse))
-    except AttributeError:
-        raise exceptions.InvalidInputException(
-            f"An attribute named {attribute_name!r} does not exist on one or more members of {self!r}."
-        )
-
-
 class FilterSet(FilterContainer, set):
-    filter = _filter
-    order_by = _order_by
+    pass
 
 
 class FilterList(FilterContainer, list):
-    filter = _filter
-    order_by = _order_by
+    pass
 
 
 class FilterDict(FilterContainer, UserDict):
@@ -112,8 +85,7 @@ class FilterDict(FilterContainer, UserDict):
         satisfied.
 
         :param bool ignore_items_without_attribute:
-        :param {str: any} kwargs: keyword arguments whose keys are the name of the filter and whose values are the
-            values to filter for
+        :param {str: any} kwargs: keyword arguments whose keys are the name of the filter and whose values are the values to filter for
         :return FilterDict:
         """
         if any(not isinstance(item, Filterable) for item in self.values()):
@@ -141,8 +113,7 @@ class FilterDict(FilterContainer, UserDict):
 
         :param str attribute_name: a dot-separated (optionally nested) attribute name e.g. "a", "a.b", "a.b.c"
         :param bool reverse:
-        :raise octue.exceptions.InvalidInputException: if an attribute with the given name doesn't exist on any of the
-            FilterDict's values
+        :raise octue.exceptions.InvalidInputException: if an attribute with the given name doesn't exist on any of the FilterDict's values
         :return FilterList:
         """
         try:
