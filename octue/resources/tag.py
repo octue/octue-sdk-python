66b4924edef22fb969686e86a622c0956307c670
cortadocodes <cortado.codes@protonmail.com>
2021-06-02 22:05:39 +0100

    FIX: Restore required method

--- octue/resources/tag.py
+++ octue/resources/tag.py
@@ -1,8 +1,10 @@
+import json
 import re
 from collections import UserDict
 
 from octue.exceptions import InvalidTagException
 from octue.mixins import Serialisable
+from octue.utils.encoders import OctueJSONEncoder
 
 
 TAG_NAME_PATTERN = re.compile(r"^[a-z0-9][a-z0-9_]*(?<!_)$")
@@ -41,3 +43,16 @@ class TagDict(Serialisable, UserDict):
                     f"Invalid tag '{tag}'. Tags must contain only characters 'a-z', 'A-Z', '0-9', and '_'. They must "
                     f"not start with '_'."
                 )
+
+    def serialise(self, to_string=False, **kwargs):
+        """Serialise a TagDict to a JSON dictionary or string.
+
+        :param bool to_string:
+        :return str|dict:
+        """
+        string = json.dumps(self.data, cls=OctueJSONEncoder, sort_keys=True, indent=4, **kwargs)
+
+        if to_string:
+            return string
+
+        return json.loads(string)
