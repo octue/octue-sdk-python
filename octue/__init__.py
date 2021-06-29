ee42b6739a11dd77ddefa70716af1cb19c5a37f7
cortadocodes <cortado.codes@protonmail.com>
2021-06-29 17:49:41 +0100

    REF: Rename PLATFORM envvar to COMPUTE_PLATFORM

--- octue/__init__.py
+++ octue/__init__.py
@@ -7,9 +7,9 @@ from .runner import Runner
 __all__ = ("Runner",)
 
 
-if os.environ.get("PLATFORM") == "GOOGLE_CLOUD_RUN":
+if os.environ.get("COMPUTE_PROVIDER") == "GOOGLE_CLOUD_RUN":
     # Use a log handler with a formatter that doesn't include the timestamp in the log message context to avoid the date
     # appearing twice in the Google Cloud Run logs (Google adds its own timestamp to log messages).
     logger = apply_log_handler(__name__, formatter=FORMATTER_WITHOUT_TIMESTAMP)
 else:
-    apply_log_handler()
+    apply_log_handler(__name__)
