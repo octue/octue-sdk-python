cd6e64edbe8de6cefe012ae606537d03cf10a532
cortadocodes <cortado.codes@protonmail.com>
2021-06-29 18:38:34 +0100

    ENH: Add default of UNKNOWN to COMPUTE_PLATFORM envvar

--- octue/__init__.py
+++ octue/__init__.py
@@ -7,7 +7,7 @@ from .runner import Runner
 __all__ = ("Runner",)
 
 
-if os.environ.get("COMPUTE_PROVIDER") == "GOOGLE_CLOUD_RUN":
+if os.environ.get("COMPUTE_PROVIDER", "UNKNOWN") == "GOOGLE_CLOUD_RUN":
     # Use a log handler with a formatter that doesn't include the timestamp in the log message context to avoid the date
     # appearing twice in the Google Cloud Run logs (Google adds its own timestamp to log messages).
     logger = apply_log_handler(__name__, formatter=FORMATTER_WITHOUT_TIMESTAMP)
