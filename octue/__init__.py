fb66806c77d8e3169dc67b757ec506094d95db99
cortadocodes <cortado.codes@protonmail.com>
2021-07-05 13:29:42 +0100

    FIX: Add USE_OCTUE_LOG_HANDLER envvar to opt-in to octue log handler

--- octue/__init__.py
+++ octue/__init__.py
@@ -11,5 +11,8 @@ if os.environ.get("COMPUTE_PROVIDER", "UNKNOWN") == "GOOGLE_CLOUD_RUN":
     # Use a log handler with a formatter that doesn't include the timestamp in the log message context to avoid the date
     # appearing twice in the Google Cloud Run logs (Google adds its own timestamp to log messages).
     logger = apply_log_handler(logger_name=None, formatter=FORMATTER_WITHOUT_TIMESTAMP)
-else:
+elif int(os.environ.get("USE_OCTUE_LOG_HANDLER", "0")) == 1:
+    # Use the default log handler from this package if `USE_OCTUE_LOG_HANDLER` is 1. The default value for this is 0
+    # because `octue` is a package that is primarily imported - the importer may not want to use this log handler if
+    # they have their own.
     apply_log_handler(logger_name=None)
--- tests/__init__.py
+++ tests/__init__.py
@@ -4,3 +4,6 @@ import os
 TESTS_DIR = os.path.dirname(__file__)
 TEST_PROJECT_NAME = os.environ["TEST_PROJECT_NAME"]
 TEST_BUCKET_NAME = "octue-test-bucket"
+
+
+os.environ["USE_OCTUE_LOG_HANDLER"] = "1"
