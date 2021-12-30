1f4d0ef73ba616c171287878a37d68a0d2d32a78
cortadocodes <cortado.codes@protonmail.com>
2021-12-30 17:31:50 +0000

    REF: Remove extra wrapper function from pipeline module

--- octue/cloud/deployment/google/dataflow/pipeline.py
+++ octue/cloud/deployment/google/dataflow/pipeline.py
@@ -1,8 +1,8 @@
 import json
 import logging
 import os
-import apache_beam as beam  # Google Dataflow provides `apache_beam` as a dependency - it is not needed as a dependency for octue-sdk-python or in the Dockerfile used with Dataflow.
-from apache_beam.options.pipeline_options import PipelineOptions  # noqa
+import apache_beam as beam
+from apache_beam.options.pipeline_options import PipelineOptions
 
 from octue import REPOSITORY_ROOT
 from octue.cloud.deployment.google.dataflow.answer_question import answer_question
@@ -12,10 +12,45 @@ from octue.cloud.pub_sub import Topic
 logger = logging.getLogger(__name__)
 
 
-def run(input_topic, output_topic, beam_args=None):
-    options = PipelineOptions(beam_args, save_main_session=True, streaming=True)
+DEFAULT_IMAGE_URI = "eu.gcr.io/octue-amy/octue-sdk-python:latest"
 
-    with beam.Pipeline(options=options) as pipeline:
+
+def deploy_pipeline(
+    input_topic_name,
+    output_topic_name,
+    project_name,
+    temporary_files_cloud_path,
+    region,
+    runner="DataflowRunner",
+    image_uri=DEFAULT_IMAGE_URI,
+    *args,
+):
+    """Deploy a streaming Dataflow pipeline to Google Cloud.
+
+    :param str input_topic_name:
+    :param str output_topic_name:
+    :param str project_name:
+    :param str temporary_files_cloud_path:
+    :param str region:
+    :param str runner:
+    :param str image_uri:
+    :param args: any further arguments in command-line-option format to be passed to Apache Beam as pipeline options
+    :return None:
+    """
+    beam_args = [
+        f"--project={project_name}",
+        f"--runner={runner}",
+        f"--temp_location={temporary_files_cloud_path}",
+        f"--region={region}",
+        f"--sdk_container_image={image_uri}",
+        f"--setup_file={os.path.join(REPOSITORY_ROOT, 'setup.py')}",
+        *args,
+    ]
+
+    input_topic = Topic.generate_topic_path(project_name, input_topic_name)
+    output_topic = Topic.generate_topic_path(project_name, output_topic_name)
+
+    with beam.Pipeline(options=PipelineOptions(beam_args, save_main_session=True, streaming=True)) as pipeline:
         (
             pipeline
             | "Read from Pub/Sub" >> beam.io.ReadFromPubSub(topic=input_topic, with_attributes=True)
@@ -23,34 +58,17 @@ def run(input_topic, output_topic, beam_args=None):
             >> beam.Map(
                 lambda question: {**json.loads(question.data.decode("utf-8")), "attributes": question.attributes}
             )
-            | "Answer question" >> beam.Map(lambda question: answer_question(question, project_name="octue-amy"))
+            | "Answer question" >> beam.Map(lambda question: answer_question(question, project_name=project_name))
             | "Encode as bytes" >> beam.Map(lambda answer: json.dumps(answer).encode("utf-8"))
             | "Write to Pub/Sub" >> beam.io.WriteToPubSub(topic=output_topic)
         )
 
 
-def deploy_pipeline(input_topic, output_topic, project_name, temporary_files_cloud_path, region, runner, image_uri):
-    run(
-        input_topic=Topic.generate_topic_path(project_name, input_topic),
-        output_topic=Topic.generate_topic_path(project_name, output_topic),
-        beam_args=[
-            f"--project={project_name}",
-            f"--runner={runner}",
-            f"--temp_location={temporary_files_cloud_path}",
-            f"--region={region}",
-            f"--sdk_container_image={image_uri}",
-            f"--setup_file={os.path.join(REPOSITORY_ROOT, 'setup.py')}",
-        ],
-    )
-
-
 if __name__ == "__main__":
     deploy_pipeline(
         project_name="octue-amy",
-        input_topic="octue.services.pub-sub-dataflow-trial",
-        output_topic="octue.services.pub-sub-dataflow-trial.answers.92505713-6052-4e17-b34e-848225ab3acc",
+        input_topic_name="octue.services.pub-sub-dataflow-trial",
+        output_topic_name="octue.services.pub-sub-dataflow-trial.answers.92505713-6052-4e17-b34e-848225ab3acc",
         temporary_files_cloud_path="gs://pub-sub-dataflow-trial/temp",
         region="europe-west2",
-        runner="DataflowRunner",
-        image_uri="eu.gcr.io/octue-amy/octue-sdk-python:latest",
     )
