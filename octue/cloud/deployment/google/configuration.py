f4c1ef1bb69397c2103eed066a813ceffa1b186e
cortadocodes <cortado.codes@protonmail.com>
2022-01-04 15:55:29 +0000

    ENH: Add cloudbuild configuration creation to Octue config reader

--- octue/cloud/deployment/google/configuration.py
+++ octue/cloud/deployment/google/configuration.py
@@ -4,45 +4,86 @@ import yaml
 DEFAULT_IMAGE_URI = "eu.gcr.io/octue-amy/octue-sdk-python:latest"
 
 
-class ConfigurationReader:
+class OctueConfigurationReader:
     def __init__(self, octue_configuration_path):
         self.octue_configuration_path = octue_configuration_path
 
         with open(self.octue_configuration_path) as f:
-            self._configuration = yaml.load(f, Loader=yaml.SafeLoader)
+            self.octue_configuration = yaml.load(f, Loader=yaml.SafeLoader)
 
-    def create_knative_config_file(self, output_path):
-        knative_configuration = {
+    def create_knative_config_file(self):
+        self.knative_configuration = {
             "apiVersion": "serving.knative.dev/v1",
             "kind": "Service",
-            "metadata": {"name": self._configuration["name"]},
+            "metadata": {"name": self.octue_configuration["name"]},
             "spec": {
                 "template": {
                     "spec": {
                         "containers": [
                             {
-                                "image": self._configuration.get("image_uri", DEFAULT_IMAGE_URI),
+                                "image": self.octue_configuration.get("image_uri", DEFAULT_IMAGE_URI),
                                 "resources": {
                                     "limits": {
-                                        "memory": self._configuration.get("memory", "128Mi"),
-                                        "cpu": self._configuration.get("cpus", 1),
+                                        "memory": self.octue_configuration.get("memory", "128Mi"),
+                                        "cpu": self.octue_configuration.get("cpus", 1),
                                     }
                                 },
                             },
-                            {"env": self._configuration.get("environment_variables", [])},
+                            {"env": self.octue_configuration.get("environment_variables", [])},
                         ],
                         "timeoutSeconds": 3600,
-                        "containerConcurrency": self._configuration.get("concurrency", 80),
+                        "containerConcurrency": self.octue_configuration.get("concurrency", 80),
                     },
                     "metadata": {
                         "annotations": {
-                            "autoscaling.knative.dev/minScale": self._configuration.get("minimum_instances", 0),
-                            "autoscaling.knative.dev/maxScale": self._configuration.get("maximum_instances", 10),
+                            "autoscaling.knative.dev/minScale": self.octue_configuration.get("minimum_instances", 0),
+                            "autoscaling.knative.dev/maxScale": self.octue_configuration.get("maximum_instances", 10),
                         }
                     },
                 }
             },
         }
 
-        with open(output_path, "w") as f:
-            yaml.safe_dump(knative_configuration, f)
+    def create_cloud_build_config_file(self):
+        self.cloud_build_configuration = {
+            "steps": [
+                {
+                    "id": "Build image",
+                    "name": "gcr.io/cloud-builders/docker",
+                    "args": [
+                        "build",
+                        "--no-cache",
+                        "-t",
+                        self.octue_configuration.get("image_uri", DEFAULT_IMAGE_URI),
+                        ".",
+                        "-f",
+                        "Dockerfile",
+                    ],
+                },
+                {
+                    "id": "Push image",
+                    "name": "gcr.io/cloud-builders/docker",
+                    "args": [
+                        "push",
+                        self.octue_configuration.get("image_uri", DEFAULT_IMAGE_URI),
+                    ],
+                },
+                {
+                    "id": "Deploy image to Google Cloud Run",
+                    "name": "gcr.io/google.com/cloudsdktool/cloud-sdk:slim",
+                    "entrypoint": "gcloud",
+                    "args": [
+                        "run",
+                        "services",
+                        "update",
+                        self.octue_configuration["name"],
+                        "--platform=managed",
+                        f'--image={self.octue_configuration["image_uri"]}',
+                        "> -",
+                        f"--region={self.octue_configuration['region']}",
+                        "--quiet",
+                    ],
+                },
+            ],
+            "images": [self.octue_configuration.get("image_uri", DEFAULT_IMAGE_URI)],
+        }
