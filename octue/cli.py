159f7325e18b771c0acdf19b328165460600d7ec
cortadocodes <cortado.codes@protonmail.com>
2022-01-10 12:02:05 +0000

    ENH: Improve CLI command help text

--- octue/cli.py
+++ octue/cli.py
@@ -230,7 +230,7 @@ def start(app_dir, data_dir, config_dir, service_id, twine, timeout, delete_topi
 @click.option("--runner", type=str, default="DataflowRunner", show_default=True)
 @click.option("--image-uri", type=str, default=pipeline.DEFAULT_IMAGE_URI, show_default=True)
 def deploy_pipeline(project_name, input_topic_name, region, runner, image_uri):
-    """Deploy a Google Dataflow pipeline as a streaming job."""
+    """Deploy a service as a Google Dataflow streaming pipeline."""
     pipeline.deploy_streaming_pipeline(
         project_name=project_name,
         input_topic_name=input_topic_name,
