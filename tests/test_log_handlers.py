2b2086e02afa608c4bc0ca6b0de6d7f54c7dbd25
cortadocodes <cortado.codes@protonmail.com>
2022-01-12 14:07:27 +0000

    TST: Update log handler tests

--- tests/test_log_handlers.py
+++ tests/test_log_handlers.py
@@ -13,7 +13,7 @@ from tests.base import BaseTestCase
 
 
 class TestLogging(BaseTestCase):
-    def test_metadata_schema_without_timestamp_used_if_compute_provider_is_google_cloud_run(self):
+    def test_log_record_attributes_without_timestamp_used_if_compute_provider_is_google_cloud_run(self):
         """Test that the formatter without a timestamp is used for logging if the `COMPUTE_PROVIDER` environment
         variable is present and equal to "GOOGLE_CLOUD_RUN", and `USE_OCTUE_LOG_HANDLER` is equal to "1".
         """
@@ -28,9 +28,9 @@ class TestLogging(BaseTestCase):
             include_thread_name=False,
         )
 
-    def test_metadata_schema_with_timestamp_used_if_compute_provider_is_not_google_cloud_run(self):
+    def test_log_record_attributes_with_timestamp_used_if_compute_provider_is_not_google_cloud_run(self):
         """Test that the formatter without a timestamp is used for logging if the `COMPUTE_PROVIDER` environment
-        variable is present and equal to "GOOGLE_CLOUD_RUN", and `USE_OCTUE_LOG_HANDLER` is equal to "1".
+        variable is present and not equal to "GOOGLE_CLOUD_RUN", and `USE_OCTUE_LOG_HANDLER` is equal to "1".
         """
         with mock.patch.dict(os.environ, USE_OCTUE_LOG_HANDLER="1", COMPUTE_PROVIDER="BLAH"):
             with mock.patch("octue.log_handlers.create_octue_formatter") as create_octue_formatter:
@@ -69,16 +69,16 @@ class TestLogging(BaseTestCase):
 
         mock_apply_log_handler.assert_called()
 
-    def test_extra_log_metadata_is_included_if_relevant_environment_variables_provided(self):
-        """Test that the expected extra log metadata is included in the log context if the relevant environment
-        variables are provided.
+    def test_extra_log_record_attributes_are_included_if_relevant_environment_variables_provided(self):
+        """Test that the expected extra log record attributes are included in the log context if the relevant
+        environment variables are provided.
         """
         with mock.patch.dict(
             os.environ,
             USE_OCTUE_LOG_HANDLER="1",
-            INCLUDE_LINE_NUMBER_LOG_METADATA="1",
-            INCLUDE_PROCESS_NAME_LOG_METADATA="1",
-            INCLUDE_THREAD_NAME_LOG_METADATA="1",
+            INCLUDE_LINE_NUMBER_IN_LOGS="1",
+            INCLUDE_PROCESS_NAME_IN_LOGS="1",
+            INCLUDE_THREAD_NAME_IN_LOGS="1",
         ):
             with mock.patch("octue.log_handlers.apply_log_handler") as mock_apply_log_handler:
                 importlib.reload(sys.modules["octue"])
