25fdff0522d06dbf6c6841070fce43bb1efe607d
cortadocodes <cortado.codes@protonmail.com>
2022-01-25 14:50:51 +0000

    FIX: Add missing return statement

--- octue/cloud/deployment/google/dataflow/deployer.py
+++ octue/cloud/deployment/google/dataflow/deployer.py
@@ -59,6 +59,7 @@ class DataflowDeployer(BaseDeployer):
         if dataflow_job_only:
             self._deploy_streaming_dataflow_job(update=update)
             print(self.success_message)
+            return
 
         self._generate_cloud_build_configuration(no_cache=no_cache)
         self._create_build_trigger(update=update)
