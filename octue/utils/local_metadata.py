fa86a7e3da284cbfcc77d7a08e8d5038c4be89b0
cortadocodes <cortado.codes@protonmail.com>
2022-04-05 15:41:55 +0100

    ENH: Log a warning if .octue files are incorrectly formatted

--- octue/utils/local_metadata.py
+++ octue/utils/local_metadata.py
@@ -1,7 +1,11 @@
 import json
+import logging
 import os
 
 from octue.utils.decoders import OctueJSONDecoder
+
+
+logger = logging.getLogger(__name__)
 
 
 LOCAL_METADATA_FILENAME = ".octue"
@@ -20,4 +24,8 @@ def load_local_metadata_file(path):
         try:
             return json.load(f, cls=OctueJSONDecoder)
         except json.decoder.JSONDecodeError:
+            logger.warning(
+                f"The {LOCAL_METADATA_FILENAME!r} metadata file at {path!r} is incorrectly formatted so no metadata "
+                f"can be read from it. Please fix or delete it."
+            )
             return {}
