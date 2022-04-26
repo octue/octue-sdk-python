f847457341b650605675b4feaf888d039b3526a7
cortadocodes <cortado.codes@protonmail.com>
2022-04-26 15:38:10 +0100

    ENH: Use colourblind colour palette for log messages

--- octue/log_handlers.py
+++ octue/log_handlers.py
@@ -10,8 +10,8 @@ from octue.utils.colour import colourise
 LOG_RECORD_ATTRIBUTES_WITH_TIMESTAMP = ["%(asctime)s", "%(levelname)s", "%(name)s"]
 LOG_RECORD_ATTRIBUTES_WITHOUT_TIMESTAMP = LOG_RECORD_ATTRIBUTES_WITH_TIMESTAMP[1:]
 
-# "tab10" colours from Seaborn.
-COLOUR_PALETTE = ["1f77b4", "ff7f0e", "2ca02c", "d62728", "9467bd", "8c564b", "e377c2", "7f7f7f", "bcbd22", "17becf"]
+# "colorblind" colour palette from seaborn/matplotlib.
+COLOUR_PALETTE = ["0173b2", "de8f05", "029e73", "d55e00", "cc78bc", "ca9161", "fbafe4", "949494", "ece133", "56b4e9"]
 DEFAULT_FOREGROUND_COLOUR = COLOUR_PALETTE[0]
 
 
