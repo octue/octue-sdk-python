6eafd9822a513b5c18af41b385e92ec9e624554c
cortadocodes <cortado.codes@protonmail.com>
2022-04-27 11:50:07 +0100

    TST: Use Child instead of Service in cloud run deployment test

--- tests/cloud/deployment/google/cloud_run/test_cloud_run_deployment.py
+++ tests/cloud/deployment/google/cloud_run/test_cloud_run_deployment.py
@@ -3,38 +3,31 @@ import unittest
 from unittest import TestCase
 
 import twined.exceptions
-from octue.cloud.pub_sub.service import Service
-from octue.resources.service_backends import GCPPubSubBackend
+from octue.resources import Child
 
 
 @unittest.skipUnless(
-    condition=os.getenv("RUN_DEPLOYMENT_TESTS", "").lower() == "1",
+    condition=os.getenv("RUN_DEPLOYMENT_TESTS", "0").lower() == "1",
     reason="'RUN_DEPLOYMENT_TESTS' environment variable is False or not present.",
 )
 class TestCloudRunDeployment(TestCase):
     # This is the service ID of the example service deployed to Google Cloud Run.
-    EXAMPLE_SERVICE_ID = "octue.services.afbf37e3-7650-4e79-bc8e-37c0c26eae13"
+    child = Child(
+        name="example-cloud-run-service",
+        id="octue.services.afbf37e3-7650-4e79-bc8e-37c0c26eae13",
+        backend={"name": "GCPPubSubBackend", "project_name": os.environ["TEST_PROJECT_NAME"]},
+    )
 
     def test_cloud_run_deployment_forwards_exceptions_to_asking_service(self):
         """Test that exceptions raised in the (remote) responding service are forwarded to and raised by the asker."""
-        asker = Service(backend=GCPPubSubBackend(project_name=os.environ["TEST_PROJECT_NAME"]))
-        subscription, _ = asker.ask(service_id=self.EXAMPLE_SERVICE_ID, input_values={"invalid_input_data": "hello"})
-
         with self.assertRaises(twined.exceptions.InvalidValuesContents):
-            asker.wait_for_answer(subscription)
+            self.child.ask(input_values={"invalid_input_data": "hello"})
 
     def test_cloud_run_deployment(self):
         """Test that the Google Cloud Run example deployment works, providing a service that can be asked questions and
         send responses.
         """
-        parent = Service(backend=GCPPubSubBackend(project_name=os.environ["TEST_PROJECT_NAME"]))
-
-        subscription, _ = parent.ask(
-            service_id=self.EXAMPLE_SERVICE_ID,
-            input_values={"n_iterations": 3},
-        )
-
-        answer = parent.wait_for_answer(subscription)
+        answer = self.child.ask(input_values={"n_iterations": 3})
 
         # Check the output values.
         self.assertEqual(answer["output_values"], [1, 2, 3, 4, 5])
