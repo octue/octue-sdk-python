5011cb687e0eb2011b205e946a4518d56bda5ad7
cortadocodes <cortado.codes@protonmail.com>
2022-04-28 16:19:14 +0100

    DOC: Update Datafile docstrings
    skipci

--- octue/resources/datafile.py
+++ octue/resources/datafile.py
@@ -37,20 +37,19 @@ OCTUE_METADATA_NAMESPACE = "octue"
 
 
 class Datafile(Labelable, Taggable, Serialisable, Identifiable, Hashable, Filterable):
-    """A representation of a data file on the Octue system. If the given path is a cloud path, the datafile's metadata
-    is pulled from the given cloud location, and any conflicting parameters (see the `Datafile.metadata` method
-    description for the parameter names concerned) are ignored. The metadata of cloud datafiles can be changed using
-    the `Datafile.update_metadata` method, but not during instantiation.
+    """A representation of a data file on the Octue system. Metadata for the file is obtained from its corresponding
+    cloud object or a local `.octue` metadata file, if present. If no stored metadata is available, it can be set during
+    or after instantiation.
 
     :param str|None path: The path of this file locally or in the cloud, which may include folders or subfolders, within the dataset
     :param str|None local_path: If a cloud path is given as the `path` parameter, this is the path to an existing local file that is known to be in sync with the cloud object
     :param str|None cloud_path: If a local path is given for the `path` parameter, this is a cloud path to keep in sync with the local file
     :param datetime.datetime|int|float|None timestamp: A posix timestamp associated with the file, in seconds since epoch, typically when it was created but could relate to a relevant time point for the data
     :param str id: The Universally Unique ID of this file (checked to be valid if not None, generated if None)
-    :param dict|TagDict tags: key-value pairs with string keys conforming to the Octue tag format (see TagDict)
-    :param iter(str) labels: Space-separated string of labels relevant to this file
+    :param dict|octue.resources.tag.TagDict|None tags: key-value pairs with string keys conforming to the Octue tag format (see TagDict)
+    :param iter(str)|octue.resources.label.LabelSet|None labels: Space-separated string of labels relevant to this file
     :param str mode: if using as a context manager, open the datafile for reading/editing in this mode (the mode options are the same as for the builtin open function)
-    :param bool update_cloud_metadata: if using as a context manager and this is True, update the cloud metadata of the datafile when the context is exited
+    :param bool update_cloud_metadata: if using as a context manager and this is `True`, update the cloud metadata of the datafile when the context is exited
     :param bool ignore_stored_metadata: if `True`, ignore any metadata stored for this datafile locally or in the cloud and use whatever is given at instantiation
     :return None:
     """
