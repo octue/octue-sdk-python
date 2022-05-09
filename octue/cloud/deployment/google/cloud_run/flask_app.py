41cb05828bcd2803f196ba5fd3248aa9333803eb
cortadocodes <cortado.codes@protonmail.com>
2022-05-09 17:48:40 +0100

    FIX: Keep list of questions delivered to Cloud Run

--- octue/cloud/deployment/google/cloud_run/flask_app.py
+++ octue/cloud/deployment/google/cloud_run/flask_app.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 from flask import Flask, request
@@ -20,14 +21,6 @@ def index():
     if not envelope:
         return _log_bad_request_and_return_400_response("No Pub/Sub message received.")
 
-    # Acknowledge questions that are redelivered to stop further redelivery and redundant processing.
-    if envelope.get("deliveryAttempt", 0) > 1:
-        logger.info(
-            "This question has already been received by the service. It will now be acknowledged to prevent further "
-            "redundant redelivery."
-        )
-        return ("", 204)
-
     if not isinstance(envelope, dict) or "message" not in envelope:
         return _log_bad_request_and_return_400_response("Invalid Pub/Sub message format.")
 
@@ -35,6 +28,17 @@ def index():
 
     if "data" not in question or "attributes" not in question or "question_uuid" not in question["attributes"]:
         return _log_bad_request_and_return_400_response("Invalid Pub/Sub message format.")
+
+    question_uuid = question["attributes"]["question_uuid"]
+    delivered_questions = _load_metadata_file()
+
+    # Acknowledge questions that are redelivered to stop further redelivery and redundant processing.
+    if question_uuid in delivered_questions:
+        return ("", 204)
+
+    # Otherwise add the question UUID to the set.
+    delivered_questions.add(question_uuid)
+    _save_metadata_file(delivered_questions)
 
     project_name = envelope["subscription"].split("/")[1]
     answer_question(question=question, project_name=project_name)
@@ -49,3 +53,18 @@ def _log_bad_request_and_return_400_response(message):
     """
     logger.error(message)
     return (f"Bad Request: {message}", 400)
+
+
+def _load_metadata_file():
+    try:
+        with open(".octue") as f:
+            return set(json.load(f))
+
+    except Exception as e:
+        logger.exception(e)
+        return []
+
+
+def _save_metadata_file(data):
+    with open(".octue", "w") as f:
+        json.dump(data, f)
