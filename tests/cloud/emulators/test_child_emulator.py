0e96f7146541aeb10aa4339029fa5e5f1686e7c1
cortadocodes <cortado.codes@protonmail.com>
2022-07-28 17:33:12 +0100

    TST: Test ChildEmulator with exceptions and monitor messages

--- tests/cloud/emulators/test_child_emulator.py
+++ tests/cloud/emulators/test_child_emulator.py
@@ -17,6 +17,16 @@ class TestChildEmulator(BaseTestCase):
         result = child_emulator.ask(input_values={"hello": "world"})
         self.assertEqual(result, {"output_values": [1, 2, 3, 4], "output_manifest": None})
 
+    def test_empty_output_returned_if_no_result_included_in_messages(self):
+        child_emulator = ChildEmulator(
+            id="emulated-child",
+            backend={"name": "GCPPubSubBackend", "project_name": "blah"},
+            messages=[],
+        )
+
+        result = child_emulator.ask(input_values={"hello": "world"})
+        self.assertEqual(result, {"output_values": None, "output_manifest": None})
+
     def test_with_logs_and_result_message(self):
         messages = [
             {
@@ -43,3 +53,33 @@ class TestChildEmulator(BaseTestCase):
 
         self.assertIn("Starting analysis.", logging_context.output[4])
         self.assertIn("Finishing analysis.", logging_context.output[5])
+
+    def test_with_exception(self):
+        messages = [{"type": "exception", "content": TypeError("This simulates an error in the child.")}]
+
+        child_emulator = ChildEmulator(
+            id="emulated-child",
+            backend={"name": "GCPPubSubBackend", "project_name": "blah"},
+            messages=messages,
+        )
+
+        with self.assertRaises(TypeError):
+            child_emulator.ask(input_values={"hello": "world"})
+
+    def test_with_monitor_message(self):
+        messages = [{"type": "monitor_message", "content": "A sample monitor message."}]
+
+        child_emulator = ChildEmulator(
+            id="emulated-child",
+            backend={"name": "GCPPubSubBackend", "project_name": "blah"},
+            messages=messages,
+        )
+
+        monitor_messages = []
+
+        child_emulator.ask(
+            input_values={"hello": "world"}, handle_monitor_message=lambda value: monitor_messages.append(value)
+        )
+
+        # Check that the monitor message handler has worked.
+        self.assertEqual(monitor_messages, ["A sample monitor message."])
