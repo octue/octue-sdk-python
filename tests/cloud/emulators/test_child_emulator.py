196a9ab9fc6de985f2911ba79e4804cba17a90ef
cortadocodes <cortado.codes@protonmail.com>
2022-07-30 15:18:07 +0100

    TST: Test message validation in ChildEmulator

--- tests/cloud/emulators/test_child_emulator.py
+++ tests/cloud/emulators/test_child_emulator.py
@@ -7,6 +7,29 @@ class TestChildEmulator(BaseTestCase):
 
     BACKEND = {"name": "GCPPubSubBackend", "project_name": "blah"}
 
+    def test_ask_with_invalid_messages(self):
+        """Test that invalid messages fail validation."""
+        messages = [{"message": "hello"}]
+
+        child_emulator = ChildEmulator(id="emulated-child", backend=self.BACKEND, messages=messages)
+
+        with self.assertRaises(ValueError):
+            child_emulator.ask(input_values={"hello": "world"})
+
+    def test_ask_with_invalid_result(self):
+        """Test that an invalid result fails validation."""
+        messages = [
+            {
+                "type": "result",
+                "content": {"wrong": "keys"},
+            },
+        ]
+
+        child_emulator = ChildEmulator(id="emulated-child", backend=self.BACKEND, messages=messages)
+
+        with self.assertRaises(ValueError):
+            child_emulator.ask(input_values={"hello": "world"})
+
     def test_ask_with_result_message(self):
         """Test that result messages are returned by the emulator's ask method."""
         output_manifest = Manifest()
@@ -30,6 +53,20 @@ class TestChildEmulator(BaseTestCase):
         result = child_emulator.ask(input_values={"hello": "world"})
         self.assertEqual(result, {"output_values": None, "output_manifest": None})
 
+    def test_ask_with_invalid_log_record(self):
+        """Test that an invalid log record representation fails validation."""
+        messages = [
+            {
+                "type": "log_record",
+                "content": [1, 2, 3],
+            },
+        ]
+
+        child_emulator = ChildEmulator(id="emulated-child", backend=self.BACKEND, messages=messages)
+
+        with self.assertRaises(TypeError):
+            child_emulator.ask(input_values={"hello": "world"})
+
     def test_ask_with_logs(self):
         """Test that log records can be handled by the emulator."""
         messages = [
@@ -50,6 +87,20 @@ class TestChildEmulator(BaseTestCase):
 
         self.assertIn("Starting analysis.", logging_context.output[4])
         self.assertIn("Finishing analysis.", logging_context.output[5])
+
+    def test_ask_with_invalid_exception(self):
+        """Test that an invalid exception fails validation."""
+        messages = [
+            {
+                "type": "exception",
+                "content": "not an exception",
+            },
+        ]
+
+        child_emulator = ChildEmulator(id="emulated-child", backend=self.BACKEND, messages=messages)
+
+        with self.assertRaises(TypeError):
+            child_emulator.ask(input_values={"hello": "world"})
 
     def test_ask_with_exception(self):
         """Test that exceptions are raised by the emulator."""
