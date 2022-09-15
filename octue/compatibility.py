227cdd8666fc98d41c05bcf4d17ff72fb9b72d07
cortadocodes <cortado.codes@protonmail.com>
2022-09-15 13:32:15 +0100

    ENH: Add latest known incompatible version info since version 0.27.1

--- octue/compatibility.py
+++ octue/compatibility.py
@@ -1,7 +1,37 @@
 from packaging.version import Version
 
 
-LATEST_KNOWN_INCOMPATIBLE_VERSIONS = {"0.27.1": Version("0.24.1")}
+VERSION_0_24_1 = Version("0.24.1")
+
+
+LATEST_KNOWN_INCOMPATIBLE_VERSIONS = {
+    "0.35.1": VERSION_0_24_1,
+    "0.35.0": VERSION_0_24_1,
+    "0.34.1": VERSION_0_24_1,
+    "0.34.0": VERSION_0_24_1,
+    "0.33.0": VERSION_0_24_1,
+    "0.32.0": VERSION_0_24_1,
+    "0.31.0": VERSION_0_24_1,
+    "0.30.0": VERSION_0_24_1,
+    "0.29.11": VERSION_0_24_1,
+    "0.29.10": VERSION_0_24_1,
+    "0.29.9": VERSION_0_24_1,
+    "0.29.8": VERSION_0_24_1,
+    "0.29.7": VERSION_0_24_1,
+    "0.29.6": VERSION_0_24_1,
+    "0.29.5": VERSION_0_24_1,
+    "0.29.4": VERSION_0_24_1,
+    "0.29.3": VERSION_0_24_1,
+    "0.29.2": VERSION_0_24_1,
+    "0.29.1": VERSION_0_24_1,
+    "0.29.0": VERSION_0_24_1,
+    "0.28.2": VERSION_0_24_1,
+    "0.28.1": VERSION_0_24_1,
+    "0.28.0": VERSION_0_24_1,
+    "0.27.3": VERSION_0_24_1,
+    "0.27.2": VERSION_0_24_1,
+    "0.27.1": VERSION_0_24_1,
+}
 
 
 def is_compatible(version_0, version_1):
