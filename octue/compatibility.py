c122713bee2b80a26faf39a8307637a1687fae05
cortadocodes <cortado.codes@protonmail.com>
2022-09-20 19:18:00 +0100

    ENH: Use empirical version compatibility data

--- octue/compatibility.py
+++ octue/compatibility.py
@@ -1,65 +1,32 @@
+import json
 import logging
-
-from packaging.version import Version
+import os
 
 
 logger = logging.getLogger(__name__)
 
-VERSION_0_24_1 = Version("0.24.1")
+
+with open(os.path.join(os.path.dirname(__file__), "metadata", "version_compatibilities.json")) as f:
+    VERSION_COMPATIBILITIES = json.load(f)
 
 
-LATEST_KNOWN_INCOMPATIBLE_VERSIONS = {
-    "0.35.1": VERSION_0_24_1,
-    "0.35.0": VERSION_0_24_1,
-    "0.34.1": VERSION_0_24_1,
-    "0.34.0": VERSION_0_24_1,
-    "0.33.0": VERSION_0_24_1,
-    "0.32.0": VERSION_0_24_1,
-    "0.31.0": VERSION_0_24_1,
-    "0.30.0": VERSION_0_24_1,
-    "0.29.11": VERSION_0_24_1,
-    "0.29.10": VERSION_0_24_1,
-    "0.29.9": VERSION_0_24_1,
-    "0.29.8": VERSION_0_24_1,
-    "0.29.7": VERSION_0_24_1,
-    "0.29.6": VERSION_0_24_1,
-    "0.29.5": VERSION_0_24_1,
-    "0.29.4": VERSION_0_24_1,
-    "0.29.3": VERSION_0_24_1,
-    "0.29.2": VERSION_0_24_1,
-    "0.29.1": VERSION_0_24_1,
-    "0.29.0": VERSION_0_24_1,
-    "0.28.2": VERSION_0_24_1,
-    "0.28.1": VERSION_0_24_1,
-    "0.28.0": VERSION_0_24_1,
-    "0.27.3": VERSION_0_24_1,
-    "0.27.2": VERSION_0_24_1,
-    "0.27.1": VERSION_0_24_1,
-}
+def is_compatible(parent_version, child_version):
+    """Check if two versions of `octue` are compatible according to empirical testing. The versions are treated as
+    compatible if there's no information on one of the versions, but a warning is also issued.
 
-
-def is_compatible(version_0, version_1):
-    """Check if two versions of `octue` are compatible. The versions are compatible if:
-    - The versions are the same
-    - The lower version is above the latest known incompatible version for the higher version
-
-    The versions are treated as incompatible if no data is available on the higher version's compatibility.
-
-    :param str version_0:
-    :param str version_1:
+    :param str parent_version: the semantic version of the parent
+    :param str child_version: the semantic version of the child
     :return bool:
     """
-    if version_0 == version_1:
+    if parent_version not in VERSION_COMPATIBILITIES or child_version not in VERSION_COMPATIBILITIES[parent_version]:
+        logger.warning(
+            "No data on compatibility of parent version %s and child version %s.",
+            parent_version,
+            child_version,
+        )
         return True
 
-    lower_version, higher_version = sorted([Version(version_0), Version(version_1)])
-
-    latest_incompatible_version = LATEST_KNOWN_INCOMPATIBLE_VERSIONS.get(str(higher_version))
-
-    if not latest_incompatible_version:
-        return False
-
-    return lower_version > latest_incompatible_version
+    return VERSION_COMPATIBILITIES[parent_version][child_version]
 
 
 def warn_if_incompatible(local_sdk_version, remote_sdk_version, perspective):
