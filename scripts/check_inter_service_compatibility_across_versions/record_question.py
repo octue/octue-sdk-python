52e74b607b8036e2ae1f9fe75782e543b01317e9
cortadocodes <cortado.codes@protonmail.com>
2022-09-20 11:59:05 +0100

    REF: Clean up question recording script

--- scripts/check_inter_service_compatibility_across_versions/record_question.py
+++ scripts/check_inter_service_compatibility_across_versions/record_question.py
@@ -11,6 +11,7 @@ from octue.resources.service_backends import GCPPubSubBackend
 from octue.utils.encoders import OctueJSONEncoder
 
 
+# Facilitate importing `MockService` across a wide range of previous versions of `octue`.
 try:
     from octue.cloud.emulators._pub_sub import MockService
 except ModuleNotFoundError:
@@ -32,10 +33,18 @@ class QuestionRecorder:
 
 
 def record_question():
-    backend = GCPPubSubBackend(project_name="octue-amy")
-    child = MockService(backend=backend, run_function=lambda: None)
+    """Record a question produced by the current version of `octue` to the file at `RECORDING_FILE`. The question is
+    recorded at the point of publishing to Pub/Sub.
+
+    :return None:
+    """
+    backend = GCPPubSubBackend(project_name="my-project")
+    child = MockService(backend=backend)
+
+    # Avoid the mock child answering the question (only the question is needed here, not the response).
     child.answer = lambda *args, **kwargs: None
-    parent = MockService(backend=backend, run_function=lambda: None, children={child.id: child})
+
+    parent = MockService(backend=backend, children={child.id: child})
 
     path = tempfile.NamedTemporaryFile().name
 
@@ -51,40 +60,39 @@ def record_question():
         }
     )
 
+    service_patcher = ServicePatcher()
+    publish_patch, question_recorder = _get_and_start_publish_patch()
+    service_patcher.patches.append(publish_patch)
+
     with ServicePatcher():
-        publish_patch, question_recorder = _get_and_start_publish_patch()
+        child.serve()
 
-        try:
-            child.serve()
+        parent.ask(
+            child.id,
+            input_values={"height": 4, "width": 72},
+            input_manifest=input_manifest,
+            allow_local_files=True,
+        )
 
-            try:
-                parent.ask(
-                    child.id,
-                    input_values={"height": 4, "width": 72},
-                    input_manifest=input_manifest,
-                    allow_local_files=True,
-                )
-
-            except KeyError:
-                pass
-
-        finally:
-            publish_patch.stop()
+    serialised_question = json.dumps(
+        {
+            "parent_sdk_version": pkg_resources.get_distribution("octue").version,
+            "question": question_recorder.question,
+        },
+        cls=OctueJSONEncoder,
+    )
 
     with open(RECORDING_FILE, "a") as f:
-        f.write(
-            json.dumps(
-                {
-                    "parent_sdk_version": pkg_resources.get_distribution("octue").version,
-                    "question": question_recorder.question,
-                },
-                cls=OctueJSONEncoder,
-            )
-            + "\n"
-        )
+        f.write(serialised_question + "\n")
 
 
 def _get_and_start_publish_patch():
+    """Patch the mock publisher's `publish` method with a `QuestionRecorder` instance so questions are accessible in
+    their just-before-sending form. This function facilitates patching `MockPublisher` across a wide range of previous
+    versions of `octue`.
+
+    :return (unittest.mock._patch, QuestionRecorder): the patch and the recorder are returned
+    """
     try:
         publish_patch = patch("octue.cloud.emulators._pub_sub.MockPublisher.publish", QuestionRecorder())
         return publish_patch, publish_patch.start()
