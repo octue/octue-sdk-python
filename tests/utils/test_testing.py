3c511eefc19bb693796d7a34c86efb261ee7c1df
cortadocodes <cortado.codes@protonmail.com>
2022-10-27 11:43:57 +0100

    TST: Test loading test fixture in serialised form

--- tests/utils/test_testing.py
+++ tests/utils/test_testing.py
@@ -20,3 +20,22 @@ class TestTesting(TestCase):
         self.assertIn("configuration_dataset", configuration_manifest.datasets)
         self.assertEqual(input_values, {"hello": "world"})
         self.assertIn("input_dataset", input_manifest.datasets)
+
+    def test_load_test_fixture_from_downloaded_crash_diagnostics_serialised(self):
+        """Test that loading a test fixture from downloaded crash diagnostics in serialised form works."""
+        configuration_values, configuration_manifest, input_values, input_manifest = load_test_fixture(
+            path=TEST_CRASH_DIAGNOSTICS_PATH,
+            serialise=True,
+        )
+
+        with open(os.path.join(TEST_CRASH_DIAGNOSTICS_PATH, "configuration_values.json")) as f:
+            self.assertEqual(configuration_values, f.read())
+
+        with open(os.path.join(TEST_CRASH_DIAGNOSTICS_PATH, "configuration_manifest.json")) as f:
+            self.assertEqual(configuration_manifest, f.read())
+
+        with open(os.path.join(TEST_CRASH_DIAGNOSTICS_PATH, "input_values.json")) as f:
+            self.assertEqual(input_values, f.read())
+
+        with open(os.path.join(TEST_CRASH_DIAGNOSTICS_PATH, "input_manifest.json")) as f:
+            self.assertEqual(input_manifest, f.read())
