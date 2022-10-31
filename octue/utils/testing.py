c40379f81d73f544195bb50a22bd8dd9dc5998d3
cortadocodes <cortado.codes@protonmail.com>
2022-10-31 19:57:44 +0000

    ENH: Remove `serialise` option from `load_test_fixture`

--- octue/utils/testing.py
+++ octue/utils/testing.py
@@ -3,10 +3,9 @@ import os
 
 from octue.cloud.emulators.child import ChildEmulator
 from octue.resources import Manifest
-from octue.utils.decoders import OctueJSONDecoder
 
 
-def load_test_fixture(path, serialise=True):
+def load_test_fixture(path):
     """Load a test fixture from the given path to a local directory created by the `octue get-crash-diagnostics` CLI
     command. The configuration values, configuration manifest, input values, and input manifest are returned if
     available. A list of child emulators is returned if the service has children and asked questions to any of them.
@@ -14,44 +13,36 @@ def load_test_fixture(path, serialise=True):
     questions were asked by the service during its analysis.
 
     :param str path: the path to a local directory containing downloaded crash diagnostics data
-    :param bool serialise: if `True`, return the values and manifests in their serialised form.
     :return (any|str|None, octue.resources.manifest.Manifest|str|None, any|str|None, octue.resources.manifest.Manifest|str|None, list(octue.cloud.emulators.child.ChildEmulator)): the configuration values, configuration manifest, input values, input manifest (each if available), and a list of child emulators corresponding to the questions asked by the service
     """
-    paths = {
-        name: os.path.join(path, name + ".json")
-        for name in ("configuration_values", "configuration_manifest", "input_values", "input_manifest")
-    }
+    return (
+        _load_values(path, "configuration"),
+        _load_manifest(path, "configuration"),
+        _load_values(path, "input"),
+        _load_manifest(path, "input"),
+        _load_child_emulators(path),
+    )
 
-    outputs = {"values": {}, "manifest": {}}
 
-    # Load configuration and input data.
-    for name, data_path in paths.items():
-        if not os.path.exists(data_path):
-            continue
+def _load_values(path, stage):
+    path = os.path.join(path, f"{stage}_values.json")
 
-        stage, data_type = name.split("_")
+    if not os.path.exists(path):
+        return
 
-        with open(data_path) as f:
-            outputs[data_type][stage] = f.read()
+    with open(path) as f:
+        return json.load(f)
 
-    # Deserialise configuration and input data if required.
-    if not serialise:
-        for name, data in outputs["values"].items():
-            outputs["values"][name] = json.loads(data, cls=OctueJSONDecoder)
 
-        for name, manifest in outputs["manifest"].items():
-            outputs["manifest"][name] = Manifest.deserialise(manifest, from_string=True)
+def _load_manifest(path, stage):
+    path = os.path.join(path, f"{stage}_manifest.json")
 
-    # Create an emulated child for each question asked by the service.
+    if os.path.exists(path):
+        return Manifest.from_file(path)
+
+
+def _load_child_emulators(path):
     with open(os.path.join(path, "questions.json")) as f:
         questions = json.load(f)
 
-    child_emulators = [ChildEmulator(id=question["id"], messages=question["messages"]) for question in questions]
-
-    return (
-        outputs["values"].get("configuration"),
-        outputs["manifest"].get("configuration"),
-        outputs["values"].get("input"),
-        outputs["manifest"].get("input"),
-        child_emulators,
-    )
+    return tuple(ChildEmulator(id=question["id"], messages=question["messages"]) for question in questions)
--- tests/utils/test_testing.py
+++ tests/utils/test_testing.py
@@ -12,44 +12,11 @@ class TestTesting(TestCase):
     def test_load_test_fixture_from_downloaded_crash_diagnostics(self):
         """Test that loading a test fixture from downloaded crash diagnostics works."""
         configuration_values, configuration_manifest, input_values, input_manifest, child_emulators = load_test_fixture(
-            path=TEST_CRASH_DIAGNOSTICS_PATH,
-            serialise=False,
+            path=TEST_CRASH_DIAGNOSTICS_PATH
         )
 
         self.assertEqual(configuration_values, {"getting": "ready"})
-        self.assertEqual(configuration_manifest.datasets["configuration_dataset"].tags, {"some": "metadata"})
-
         self.assertEqual(input_values, {"hello": "world"})
-        self.assertEqual(input_manifest.datasets["input_dataset"].tags, {"more": "metadata"})
-
-        self.assertEqual(len(child_emulators), 1)
-        self.assertEqual(child_emulators[0].id, "octue/my-child:latest")
-        self.assertEqual(
-            child_emulators[0].messages[2:],
-            [
-                {"type": "monitor_message", "data": '{"sample": "data"}'},
-                {"type": "result", "output_values": [1, 2, 3, 4, 5], "output_manifest": None},
-            ],
-        )
-
-    def test_load_test_fixture_from_downloaded_crash_diagnostics_serialised(self):
-        """Test that loading a test fixture from downloaded crash diagnostics in serialised form works."""
-        configuration_values, configuration_manifest, input_values, input_manifest, child_emulators = load_test_fixture(
-            path=TEST_CRASH_DIAGNOSTICS_PATH,
-            serialise=True,
-        )
-
-        with open(os.path.join(TEST_CRASH_DIAGNOSTICS_PATH, "configuration_values.json")) as f:
-            self.assertEqual(configuration_values, f.read())
-
-        with open(os.path.join(TEST_CRASH_DIAGNOSTICS_PATH, "configuration_manifest.json")) as f:
-            self.assertEqual(configuration_manifest, f.read())
-
-        with open(os.path.join(TEST_CRASH_DIAGNOSTICS_PATH, "input_values.json")) as f:
-            self.assertEqual(input_values, f.read())
-
-        with open(os.path.join(TEST_CRASH_DIAGNOSTICS_PATH, "input_manifest.json")) as f:
-            self.assertEqual(input_manifest, f.read())
 
         self.assertEqual(len(child_emulators), 1)
         self.assertEqual(child_emulators[0].id, "octue/my-child:latest")
