029a27d4cf57ded5e581fd7f87ffbc9cee1e9806
cortadocodes <cortado.codes@protonmail.com>
2022-11-01 15:09:49 +0000

    DOC: Improve `octue.utils.testing` docstrings

--- octue/utils/testing.py
+++ octue/utils/testing.py
@@ -6,14 +6,14 @@ from octue.resources import Manifest
 
 
 def load_test_fixture_from_crash_diagnostics(path):
-    """Load a test fixture from the given path to a local directory created by the `octue get-crash-diagnostics` CLI
+    """Load a test fixture from service crash diagnostics downloaded using the `octue get-crash-diagnostics` CLI
     command. The configuration values, configuration manifest, input values, and input manifest are returned if
-    available. A list of child emulators is returned if the service has children and asked questions to any of them.
+    available. A tuple of child emulators is returned if the service has children and asked questions to any of them.
     Each child emulator corresponds to one question asked to a child. The child emulators are in the same order as the
     questions were asked by the service during its analysis.
 
     :param str path: the path to a local directory containing downloaded crash diagnostics data
-    :return (any|str|None, octue.resources.manifest.Manifest|str|None, any|str|None, octue.resources.manifest.Manifest|str|None, list(octue.cloud.emulators.child.ChildEmulator)): the configuration values, configuration manifest, input values, input manifest (each if available), and a list of child emulators corresponding to the questions asked by the service
+    :return (any|None, octue.resources.manifest.Manifest|None, any|None, octue.resources.manifest.Manifest|None, tuple(octue.cloud.emulators.child.ChildEmulator)): the configuration values, configuration manifest, input values, input manifest (each if available), and a tuple of child emulators corresponding to the questions asked by the service
     """
     return (
         _load_values(path, "configuration"),
@@ -25,6 +25,12 @@ def load_test_fixture_from_crash_diagnostics(path):
 
 
 def _load_values(path, stage):
+    """Load values from a JSON file in the directory specified by `path`.
+
+    :param str path: the path to the crash diagnostics directory
+    :param str stage: one of "configuration" or "input"
+    :return any|None: the values or `None` if no relevant file exists in the directory specified by `path`
+    """
     path = os.path.join(path, f"{stage}_values.json")
 
     if not os.path.exists(path):
@@ -35,13 +41,26 @@ def _load_values(path, stage):
 
 
 def _load_manifest(path, stage):
+    """Load a manifest from a JSON file in the directory specified by `path`.
+
+    :param str path: the path to the crash diagnostics directory
+    :param str stage: one of "configuration" or "input"
+    :return any|None: the manifest or `None` if no relevant file exists in the directory specified by `path`
+    """
     path = os.path.join(path, f"{stage}_manifest.json")
 
-    if os.path.exists(path):
-        return Manifest.from_file(path)
+    if not os.path.exists(path):
+        return
+
+    return Manifest.from_file(path)
 
 
 def _load_child_emulators(path):
+    """Load child emulators from a JSON file in the directory specified by `path`.
+
+    :param str path: the path to the crash diagnostics directory
+    :return tuple(octue.resources.child.Child):
+    """
     with open(os.path.join(path, "questions.json")) as f:
         questions = json.load(f)
 
