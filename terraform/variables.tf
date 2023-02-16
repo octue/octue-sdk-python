5a282dddddee84a6c781b3d43aec997e2ee46502
cortadocodes <cortado.codes@protonmail.com>
2023-02-16 15:38:37 +0000

    OPS: Add terraform configuration for test project

---
+++ terraform/variables.tf
@@ -0,0 +1,29 @@
+variable "organization" {
+  type = string
+  default = "octue"
+}
+
+variable "project" {
+  type    = string
+  default = "octue-sdk-python"
+}
+
+variable "project_number" {
+  type = string
+  default = "437801218871"
+}
+
+variable "region" {
+  type = string
+  default = "europe-west1"
+}
+
+variable "github_organisation" {
+  type    = string
+  default = "octue"
+}
+
+variable "credentials_file" {
+  type    = string
+  default = "gcp-credentials.json"
+}
