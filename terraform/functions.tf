ca610aa1d07558654efd159de89eaf4f9d30bf14
cortadocodes <cortado.codes@protonmail.com>
2024-04-10 17:30:15 +0100

    OPS: Use fixed event handler cloud function

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -9,7 +9,7 @@ resource "google_cloudfunctions2_function" "event_handler" {
     source {
       storage_source {
         bucket = "twined-gcp"
-        object = "event_handler/0.3.0.zip"
+        object = "event_handler/0.3.1.zip"
       }
     }
   }
