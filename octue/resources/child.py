2f36b9881030b880f881f6d5d2533da464c37db5
cortadocodes <cortado.codes@protonmail.com>
2024-04-11 15:03:39 +0100

    DOC: Update `Child` docstrings

--- octue/resources/child.py
+++ octue/resources/child.py
@@ -72,7 +72,7 @@ class Child:
         - A synchronous (ask-and-wait) question and wait for it to return an output. Questions are synchronous if
           the `push_endpoint` isn't provided and `asynchronous=False`.
         - An asynchronous (fire-and-forget) question and return immediately. To make a question asynchronous, provide
-          the `push_endpoint` argument or set `asynchronous=False`.
+          the `push_endpoint` argument or set `asynchronous=True`.
 
         :param any|None input_values: any input values for the question, conforming with the schema in the child's twine
         :param octue.resources.manifest.Manifest|None input_manifest: an input manifest of any datasets needed for the question, conforming with the schema in the child's twine
@@ -84,7 +84,7 @@ class Child:
         :param str save_diagnostics: must be one of {"SAVE_DIAGNOSTICS_OFF", "SAVE_DIAGNOSTICS_ON_CRASH", "SAVE_DIAGNOSTICS_ON"}; if turned on, allow the input values and manifest (and its datasets) to be saved by the child either all the time or just if it fails while processing them
         :param str|None question_uuid: the UUID to use for the question if a specific one is needed; a UUID is generated if not
         :param str|None push_endpoint: if answers to the question should be pushed to an endpoint, provide its URL here (the returned subscription will be a push subscription); if not, leave this as `None`
-        :param bool asynchronous: if `True`, don't create an answer subscription
+        :param bool asynchronous: if `True`, don't wait for an answer or create an answer subscription (the result and other events can be retrieved from the event store later)
         :param float timeout: time in seconds to wait for an answer before raising a timeout error
         :param float|int maximum_heartbeat_interval: the maximum amount of time (in seconds) allowed between child heartbeats before an error is raised
         :raise TimeoutError: if the timeout is exceeded while waiting for an answer
