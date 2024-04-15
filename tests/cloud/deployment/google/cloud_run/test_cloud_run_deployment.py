c3953df26d3d1dc47ab2acee8792c81e6c0873d3
cortadocodes <cortado.codes@protonmail.com>
2024-04-15 11:08:45 +0100

    TST: Test retrieving results from real asynchronous question

--- tests/cloud/deployment/google/cloud_run/test_cloud_run_deployment.py
+++ tests/cloud/deployment/google/cloud_run/test_cloud_run_deployment.py
@@ -1,9 +1,15 @@
 import os
+import time
 import unittest
+import uuid
 from unittest import TestCase
 
 import twined.exceptions
-from octue.resources import Child
+from octue.cloud.pub_sub.bigquery import get_events
+from octue.resources import Child, Manifest
+
+
+EXAMPLE_SERVICE_SRUID = "octue/example-service-cloud-run:0.4.0"
 
 
 @unittest.skipUnless(
@@ -13,7 +19,7 @@ from octue.resources import Child
 class TestCloudRunDeployment(TestCase):
     # This is the service ID of the example service deployed to Google Cloud Run.
     child = Child(
-        id="octue/example-service-cloud-run:0.4.0",
+        id=EXAMPLE_SERVICE_SRUID,
         backend={"name": "GCPPubSubBackend", "project_name": os.environ["TEST_PROJECT_NAME"]},
     )
 
@@ -36,6 +42,26 @@ class TestCloudRunDeployment(TestCase):
             self.assertEqual(f.read(), "This is some example service output.")
 
     def test_asynchronous_question(self):
-        """Test asking an asynchronous question."""
-        answer = self.child.ask(input_values={"n_iterations": 3}, asynchronous=True)
+        """Test asking an asynchronous question and retrieving the resulting events from the event store."""
+        question_uuid = str(uuid.uuid4())
+        answer = self.child.ask(input_values={"n_iterations": 3}, question_uuid=question_uuid, asynchronous=True)
         self.assertIsNone(answer)
+
+        # Wait for question to complete.
+        time.sleep(10)
+
+        events = get_events(
+            table_id="octue_sdk_python_test_dataset.service-events",
+            sender=EXAMPLE_SERVICE_SRUID,
+            question_uuid=question_uuid,
+            kind="result",
+        )
+
+        # Check the output values.
+        self.assertEqual(events[0]["event"]["output_values"], [1, 2, 3, 4, 5])
+
+        # Check that the output dataset and its files can be accessed.
+        output_manifest = Manifest.deserialise(events[0]["event"]["output_manifest"])
+
+        with output_manifest.datasets["example_dataset"].files.one() as (datafile, f):
+            self.assertEqual(f.read(), "This is some example service output.")
