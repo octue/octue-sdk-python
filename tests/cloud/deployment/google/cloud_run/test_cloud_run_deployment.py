40f23e944cfb07254aa469b53090a603e0d027e2
cortadocodes <cortado.codes@protonmail.com>
2024-04-17 14:23:13 +0100

    TST: Update deployment test

--- tests/cloud/deployment/google/cloud_run/test_cloud_run_deployment.py
+++ tests/cloud/deployment/google/cloud_run/test_cloud_run_deployment.py
@@ -8,7 +8,7 @@ from octue.cloud.pub_sub.bigquery import get_events
 from octue.resources import Child
 
 
-EXAMPLE_SERVICE_SRUID = "octue/example-service-cloud-run:0.4.0"
+EXAMPLE_SERVICE_SRUID = "octue/example-service-cloud-run:0.4.1"
 
 
 @unittest.skipUnless(
