e67a30a7c8b970fa2a2625792586cd92d1b7816e
cortadocodes <cortado.codes@protonmail.com>
2024-07-10 14:51:15 +0100

    WIP: Log registered temporary directories

--- octue/utils/files.py
+++ octue/utils/files.py
@@ -1,4 +1,8 @@
+import logging
 import tempfile
+
+
+logger = logging.getLogger(__name__)
 
 
 temporary_directories = []
@@ -10,3 +14,4 @@ class RegisteredTemporaryDirectory(tempfile.TemporaryDirectory):
     def __init__(self, suffix=None, prefix=None, dir=None, ignore_cleanup_errors=False):
         super().__init__(suffix=suffix, prefix=prefix, dir=dir, ignore_cleanup_errors=ignore_cleanup_errors)
         temporary_directories.append(self)
+        logger.warning("DIAGNOSTICS! Using temporary directory %r.", self.name)
