7970e57819c64537850ac93bd06d515d3b9b1ca7
cortadocodes <cortado.codes@protonmail.com>
2024-07-15 11:36:04 +0100

    OPS: Use new event handler function

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -9,7 +9,7 @@ resource "google_cloudfunctions2_function" "event_handler" {
     source {
       storage_source {
         bucket = "twined-gcp"
-        object = "event_handler/0.6.0.zip"
+        object = "event_handler/0.6.1.zip"
       }
     }
   }
