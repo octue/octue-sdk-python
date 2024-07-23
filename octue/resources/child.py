29289467630cc9feeec7f327536cd9fa82eba8b7
cortadocodes <cortado.codes@protonmail.com>
2024-07-23 15:23:32 +0100

    REF: Move retry logic from `Child.ask_multiple` into `Child.ask`

--- octue/resources/child.py
+++ octue/resources/child.py
@@ -2,7 +2,6 @@ import concurrent.futures
 import copy
 import logging
 import os
-import uuid
 
 from octue.cloud.pub_sub.service import Service
 from octue.resources import service_backends
@@ -70,6 +69,10 @@ class Child:
         push_endpoint=None,
         asynchronous=False,
         retry_count=0,
+        raise_errors=True,
+        max_retries=0,
+        prevent_retries_when=None,
+        log_final_retry_error=True,
         timeout=86400,
         maximum_heartbeat_interval=300,
     ):
@@ -94,68 +97,93 @@ class Child:
         :param str|None push_endpoint: if answers to the question should be pushed to an endpoint, provide its URL here (the returned subscription will be a push subscription); if not, leave this as `None`
         :param bool asynchronous: if `True`, don't wait for an answer or create an answer subscription (the result and other events can be retrieved from the event store later)
         :param int retry_count: the retry count of the question (this is zero if it's the first attempt at the question)
+        :param bool raise_errors: if `True` and the question fails, raise the error; if False, return the error
+        :param int max_retries: if `raise_errors=False` and the question fails, retry the question up to this number of times
+        :param list(type)|None prevent_retries_when: if `raise_errors=False` and the question fails, prevent retrying the question if it fails with an exception type in this list
+        :param bool log_final_retry_error: if `True`, `raise_errors=False`, and the question fails after its final retry, log the error
         :param float timeout: time in seconds to wait for an answer before raising a timeout error
         :param float|int maximum_heartbeat_interval: the maximum amount of time (in seconds) allowed between child heartbeats before an error is raised
         :raise TimeoutError: if the timeout is exceeded while waiting for an answer
-        :return dict|octue.cloud.pub_sub.subscription.Subscription|None, str: for a synchronous question, a dictionary containing the keys "output_values" and "output_manifest" from the result, and the question UUID; for a question with a push endpoint, the push subscription and the question UUID; for an asynchronous question, `None` and the question UUID
+        :raise Exception: if the question raises an error and `raise_errors=True`
+        :return dict|octue.cloud.pub_sub.subscription.Subscription|Exception|None, str: for a synchronous question, a dictionary containing the keys "output_values" and "output_manifest" from the result, and the question UUID; for a question with a push endpoint, the push subscription and the question UUID; for an asynchronous question, `None` and the question UUID
         """
-        subscription, question_uuid = self._service.ask(
-            service_id=self.id,
-            input_values=input_values,
-            input_manifest=input_manifest,
-            children=children,
-            subscribe_to_logs=subscribe_to_logs,
-            allow_local_files=allow_local_files,
-            save_diagnostics=save_diagnostics,
-            question_uuid=question_uuid,
-            parent_question_uuid=parent_question_uuid,
-            originator_question_uuid=originator_question_uuid,
-            originator=originator,
-            push_endpoint=push_endpoint,
-            asynchronous=asynchronous,
-            retry_count=retry_count,
-            timeout=timeout,
-        )
+        prevent_retries_when = prevent_retries_when or []
+
+        inputs = {
+            "input_values": input_values,
+            "input_manifest": input_manifest,
+            "children": children,
+            "subscribe_to_logs": subscribe_to_logs,
+            "allow_local_files": allow_local_files,
+            "save_diagnostics": save_diagnostics,
+            "question_uuid": question_uuid,
+            "parent_question_uuid": parent_question_uuid,
+            "originator_question_uuid": originator_question_uuid,
+            "originator": originator,
+            "push_endpoint": push_endpoint,
+            "asynchronous": asynchronous,
+            "retry_count": retry_count,
+            "timeout": timeout,
+        }
+
+        subscription, question_uuid = self._service.ask(service_id=self.id, **inputs)
 
         if push_endpoint or asynchronous:
             return subscription, question_uuid
 
-        answer = self._service.wait_for_answer(
-            subscription=subscription,
-            handle_monitor_message=handle_monitor_message,
-            record_events=record_events,
-            timeout=timeout,
-            maximum_heartbeat_interval=maximum_heartbeat_interval,
-        )
+        try:
+            answer = self._service.wait_for_answer(
+                subscription=subscription,
+                handle_monitor_message=handle_monitor_message,
+                record_events=record_events,
+                timeout=timeout,
+                maximum_heartbeat_interval=maximum_heartbeat_interval,
+            )
 
-        return answer, question_uuid
+            return answer, question_uuid
 
-    def ask_multiple(
-        self,
-        *questions,
-        raise_errors=True,
-        max_retries=0,
-        prevent_retries_when=None,
-        max_workers=None,
-        log_errors=True,
-    ):
+        except Exception as e:
+            logger.error(
+                "Question %r failed. Run 'octue get-diagnostics gs://<diagnostics-cloud-path>/%s "
+                "--download-datasets' to get the crash diagnostics.",
+                question_uuid,
+                question_uuid,
+            )
+
+            if raise_errors:
+                raise e
+
+            for retry in range(max_retries):
+                logger.info("Retrying question %r %d of %d times.", question_uuid, retry + 1, max_retries)
+
+                inputs["retry_count"] += 1
+                answer, question_uuid = self.ask(**inputs, raise_errors=False, log_final_retry_error=False)
+
+                if not isinstance(answer, Exception) or type(answer) in prevent_retries_when:
+                    return answer, question_uuid
+
+                e = answer
+
+            if log_final_retry_error:
+                logger.error(
+                    "Question %r failed after %d retries (see below for error).",
+                    question_uuid,
+                    max_retries,
+                    exc_info=e,
+                )
+
+            return e, question_uuid
+
+    def ask_multiple(self, *questions, max_workers=None):
         """Ask the child multiple questions in parallel and wait for the answers. Each question should be provided as a
         dictionary of `Child.ask` keyword arguments. If `raise_errors` is `True`, an error is raised and no answers are
         returned if any of the individual questions raise an error; if it's `False`, answers are returned for all
         successful questions while errors are returned unraised for any failed ones.
 
         :param questions: any number of questions provided as dictionaries of arguments to the `Child.ask` method
-        :param bool raise_errors: if `True`, an error is raised and no answers are returned if any of the individual questions raise an error; if `False`, answers are returned for all successful questions while errors are returned unraised for any failed ones
-        :param int max_retries: retry any questions that failed up to this number of times (note: this will have no effect unless `raise_errors=False`)
-        :param list(type)|None prevent_retries_when: prevent retrying any questions that fail with an exception type in this list (note: this will have no effect unless `raise_errors=False`)
         :param int|None max_workers: the maximum number of questions that can be asked at once; defaults to the lowest of {32, no. of CPUs + 4, and no. of questions} (see `concurrent.futures.ThreadPoolExecutor`)
-        :param bool log_errors: if `True` and `raise_errors=False`, log any errors remaining once retries are exhausted
-        :raise ValueError: if the maximum number of parallel questions is set too high
-        :raise Exception: if any question raises an error if `raise_errors` is `True`
         :return list(dict|Exception, str): the answers or caught errors of the questions, and the question UUIDs (in the same order as asked)
         """
-        prevent_retries_when = prevent_retries_when or []
-
         # Answers will come out of order, so use a dictionary to store them against their questions' original index.
         answers = {}
         n_questions = len(questions)
@@ -166,83 +194,13 @@ class Child:
             future_to_question_index_mapping = {}
 
             for i, question in enumerate(questions):
-                # Add a question UUID if not set so retries have the same one.
-                if "question_uuid" not in question:
-                    question["question_uuid"] = str(uuid.uuid4())
-
                 future = executor.submit(self.ask, **question)
                 future_to_question_index_mapping[future] = i
 
             for i, future in enumerate(concurrent.futures.as_completed(future_to_question_index_mapping)):
                 logger.info("%d of %d answers received.", i + 1, n_questions)
                 question_index = future_to_question_index_mapping[future]
-
-                try:
-                    answers[question_index] = future.result()
-                except Exception as e:
-                    if raise_errors:
-                        raise e
-
-                    question_uuid = questions[question_index]["question_uuid"]
-                    answers[question_index] = (e, question_uuid)
-
-                    logger.error(
-                        "Question %s failed. Run 'octue get-diagnostics gs://<diagnostics-cloud-path>/%s "
-                        "--download-datasets' to get the crash diagnostics.",
-                        question_uuid,
-                        question_uuid,
-                    )
-
-        for retry in range(max_retries):
-            failed_questions = self._get_failed_questions(
-                questions,
-                answers,
-                prevent_retries_when,
-                increment_retry_count=True,
-            )
-
-            if not failed_questions:
-                break
-
-            logger.info("%d questions failed - retrying %d of %d times.", len(failed_questions), retry + 1, max_retries)
-            retried_answers = self.ask_multiple(*failed_questions.values(), raise_errors=False, log_errors=False)
-
-            for question_index, answer in zip(failed_questions.keys(), retried_answers):
-                answers[question_index] = answer
-
-        if log_errors:
-            # Check for failed questions after retries completed.
-            failed_questions = self._get_failed_questions(questions, answers, prevent_retries_when)
-
-            for question_index, question in failed_questions.items():
-                logger.error(
-                    "Question %s failed after %d retries (see below for error).",
-                    question["question_uuid"],
-                    max_retries,
-                    exc_info=answers[question_index][0],
-                )
+                answers[question_index] = future.result()
 
         # Convert dictionary to list in asking order.
         return [answer[1] for answer in sorted(answers.items(), key=lambda item: item[0])]
-
-    def _get_failed_questions(self, questions, answers, prevent_retries_when, increment_retry_count=False):
-        """Get the questions that failed.
-
-        :param list(dict) questions: the list of questions that were asked
-        :param dict answers: a mapping of question index (i.e. position in the original list of questions) to question answer
-        :param list(type)|None prevent_retries_when: prevent retrying any questions that fail with an exception type in this list (note: this will have no effect unless `raise_errors=False`)
-        :param bool increment_retry_count: if `True`, increment the question retry count by 1
-        :return dict: a mapping of failed question index (i.e. position in the original list of questions) to failed question
-        """
-        failed_questions = {}
-
-        for question_index, answer in answers.items():
-            if isinstance(answer[0], Exception) and type(answer[0]) not in prevent_retries_when:
-                question = questions[question_index]
-
-                if increment_retry_count:
-                    question["retry_count"] = question.get("retry_count", 0) + 1
-
-                failed_questions[question_index] = question
-
-        return failed_questions
