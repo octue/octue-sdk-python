3d4c525192b40ade7c83023c32ced40d90a59f18
cortadocodes <cortado.codes@protonmail.com>
2025-01-13 13:07:33 +0000

    FIX: Fix event and attributes generation

--- octue/cloud/events/utils.py
+++ octue/cloud/events/utils.py
@@ -2,16 +2,14 @@ import datetime
 import uuid
 
 from octue.cloud import LOCAL_SDK_VERSION
+from octue.utils.dictionaries import make_minimal_dictionary
 
 
 def make_originator_question_event(input_values, input_manifest, parent_sruid, child_sruid, question_uuid=None):
-    question_uuid = question_uuid or uuid.uuid4()
+    question_uuid = question_uuid or str(uuid.uuid4())
 
     return {
-        "event": {
-            "input_values": input_values,
-            "input_manifest": input_manifest,
-        },
+        "event": make_minimal_dictionary(input_values=input_values, input_manifest=input_manifest, kind="question"),
         "attributes": make_attributes(
             question_uuid=question_uuid,
             parent_question_uuid=question_uuid,
@@ -20,6 +18,9 @@ def make_originator_question_event(input_values, input_manifest, parent_sruid, c
             originator=parent_sruid,
             sender=parent_sruid,
             recipient=child_sruid,
+            forward_logs=True,
+            save_diagnostics="SAVE_DIAGNOSTICS_ON",
+            sender_type="PARENT",
         ),
     }
 
@@ -30,11 +31,14 @@ def make_attributes(
     parent,
     originator,
     sender,
+    sender_type,
     recipient,
     question_uuid=None,
     retry_count=0,
+    forward_logs=None,
+    save_diagnostics=None,
 ):
-    return {
+    attributes = {
         "uuid": str(uuid.uuid4()),
         "datetime": datetime.datetime.now(tz=datetime.timezone.utc).isoformat(),
         "question_uuid": question_uuid or str(uuid.uuid4()),
@@ -43,7 +47,20 @@ def make_attributes(
         "parent": parent,
         "originator": originator,
         "sender": sender,
+        "sender_type": sender_type,
         "sender_sdk_version": LOCAL_SDK_VERSION,
         "recipient": recipient,
         "retry_count": retry_count,
     }
+
+    if sender_type == "PARENT":
+        if forward_logs is None or save_diagnostics is None:
+            raise ValueError(
+                "`forward_logs` and `save_diagnostics` must be present in the attributes if the sender type is "
+                "'PARENT'."
+            )
+
+        attributes["forward_logs"] = forward_logs
+        attributes["save_diagnostics"] = save_diagnostics
+
+    return attributes
