9081704f8fdd47f52b34ec95aa2c0fd0a3818fe5
cortadocodes <cortado.codes@protonmail.com>
2025-02-03 12:33:11 +0000

    REV: Revert "FIX: Refresh credentials before returning"
    This reverts commit 89c53622d91d5f031bb39b1426e3fda351d64828.
    
    skipci

--- octue/cloud/pub_sub/credentials.py
+++ octue/cloud/pub_sub/credentials.py
@@ -5,6 +5,6 @@ import google.auth.transport.requests
 def get_gcp_credentials():
     """Get the default credentials for Google Cloud Platform."""
     credentials, _ = google.auth.default()
-    auth_request = google.auth.transport.requests.Request()
-    credentials.refresh(auth_request)
+    # auth_request = google.auth.transport.requests.Request()
+    # credentials.refresh(auth_request)
     return credentials
