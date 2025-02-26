7adfe38469deefdb356cf9cf929f492191f2b35e
cortadocodes <cortado.codes@protonmail.com>
2025-02-26 17:49:42 +0000

    REF: Add default args and document `make_question_event`
    skipci

--- octue/cloud/events/utils.py
+++ octue/cloud/events/utils.py
@@ -3,8 +3,8 @@ from octue.utils.dictionaries import make_minimal_dictionary
 
 
 def make_question_event(
-    input_values,
-    input_manifest,
+    input_values=None,
+    input_manifest=None,
     sender=None,
     recipient=None,
     question_uuid=None,
@@ -12,13 +12,13 @@ def make_question_event(
 ):
     """Make a question event. If the `attributes` argument isn't provided, the question will be an originator question.
 
-    :param dict input_values:
-    :param octue.resources.manifest.Manifest input_manifest:
-    :param str|None sender:
-    :param str|None recipient:
-    :param str|None question_uuid:
-    :param dict|None attributes:
-    :return dict:
+    :param dict|None input_values: any input values for the question
+    :param dict|None input_manifest: an input manifest of any datasets needed for the question as a python primitive
+    :param str|None sender: the service revision unique identifier (SRUID) of the service revision sending the question
+    :param str|None recipient: the service revision unique identifier (SRUID) of the service revision the question is for
+    :param str|None question_uuid: the UUID to use for the question; if `None`, a UUID is generated
+    :param dict|None attributes: the attributes to use for the question event; if none are provided, the question will be an originator question
+    :return dict: the question event and its attributes
     """
     if not attributes:
         attributes = EventAttributes(
