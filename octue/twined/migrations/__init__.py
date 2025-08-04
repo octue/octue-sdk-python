39b2af19fd80199c9001f1ba994da9325360ba92
cortadocodes <cortado.codes@protonmail.com>
2025-08-04 11:30:56 -0400

    REF: Move migrations subpackage

--- octue/twined/migrations/__init__.py
+++ octue/twined/migrations/__init__.py
@@ -1,3 +1,3 @@
 """A subpackage containing any translations from deprecated code to new code for deprecations that haven't been phased
-out yet. See https://github.com/octue/twined/issues/102.
+out yet. See https://github.com/octue/octue-sdk-python/issues/415.
 """
