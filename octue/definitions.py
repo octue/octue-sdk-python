c4c3fd03c7800b67c2588799e19065108903aeb5
cortadocodes <cortado.codes@protonmail.com>
2025-08-04 15:59:48 -0400

    REF: Remove unused constant

--- octue/definitions.py
+++ octue/definitions.py
@@ -3,19 +3,7 @@ import importlib.metadata
 VALUES_FILENAME = "values.json"
 MANIFEST_FILENAME = "manifest.json"
 
-STRAND_FILENAME_MAP = {
-    "configuration_values": VALUES_FILENAME,
-    "configuration_manifest": MANIFEST_FILENAME,
-    "input_values": VALUES_FILENAME,
-    "input_manifest": MANIFEST_FILENAME,
-    "output_values": VALUES_FILENAME,
-    "output_manifest": MANIFEST_FILENAME,
-}
-
-# TODO this should probably be defined in twined
 OUTPUT_STRANDS = ("output_values", "output_manifest")
-
-# TODO this should probably be defined in twined
 RUN_STRANDS = ("input_values", "input_manifest", "credentials", "children")
 
 GOOGLE_COMPUTE_PROVIDERS = {"GOOGLE_CLOUD_FUNCTION"}
